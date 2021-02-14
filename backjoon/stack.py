26187dd2c68247f8f18f3f56da98a3d0aec441af
lungnahahd <jyoungjk33@gmail.com>
2021-02-14 23:08:43 +0900

    Fix_stack.py(top, pop,empty 기능)

--- backjoon/stack.py
+++ backjoon/stack.py
@@ -1,45 +1,40 @@
-
-a, b, v = input().split()
-up = int(a)
-down = int(b)
-length = int(v)
+a,b = input().split()
+print(a)
+print(b)
 
 
-if __main__ == "__main__":
-    num = input()
-    num = int(num)
-    stack = []
-    count = 0
-    top = 0
-    while count != num:
-        call = input()
-        call, param = input().split()
+num = input()
+num = int(num)
+stack = []
+count = 0
+top = 0
+while count != num:
+    call = input()
+
+    if call == "top":
+        if not stack:
+            print("-1")
+        else:
+            print(stack[0])
+    elif call == "pop":
+        if not stack:
+            print("-1")
+        else:
+            print(stack[0])
+            temp = []
+            for i in range(len(stack) + 1):
+                temp[i] = stack[i+1]
+            stack = temp
+    elif call == "size":
+        print(len(stack))
+    elif call == "empty":
+        if stack is not None:
+            print("0")
+        else:
+            print("1")
+    else :
+        call,param = input().split()
         param = int(param)
-
-        if call == "push":
-            stack[top] = param
-            top += 1
-        elif call == "pop":
-            if call is not None:
-                print(call[0])
-                temp = []
-                for i in range(len(call) + 1):
-                    temp[i] = call[i+1]
-                call = temp
-            else:
-                print("-1")
-        elif call == "size":
-            print(len(call))
-        elif call == "empty":
-            if call is not None:
-                print("0")
-            else:
-                print("1")
-        else :
-            if call is not None:
-                print(call[0])
-            else:
-                print("-1")
-
-
-        count += 1
+        stack[top] = param
+        top += 1
+    count += 1
