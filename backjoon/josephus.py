33594c389e9713aace9ac42b3e9a0479fb2a053d
lungnahahd <jyoungjk33@gmail.com>
2021-02-22 17:07:14 +0900

    요세푸스 알고리즘 완성

--- backjoon/josephus.py
+++ backjoon/josephus.py
@@ -1,18 +1,29 @@
 import sys
 input = sys.stdin.readline
 
-num = input()
+
+num, param = input().split()
 num = int(num)
-param = input()
 param = int(param)
 queue = [i + 1 for i in range(num)]
+final = [0 for i in range(num)]
 out = 0
-size = len(queue) - 1 
+size = len(queue) 
 
 for i in range(num):
     out = out + param - 1
-    if out > size:
-        out = out - size - 1
-
+    while out > size - 1:
+        out = out - size
+      
     size += -1
-    print(queue.pop(out))
+    final[i] = queue.pop(out)
+if num == 1:
+    print("<1>")
+else:
+    for i in range(num):
+        if i == 0:
+            print("<" + str(final[i]) + ", ",end="")
+        elif i == num -1:
+            print(str(final[i]) + ">",end="")
+        else:
+            print(str(final[i]) + ", ",end="")
