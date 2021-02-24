963728f5ca885d1380ad1389a1ee117398b3cfaf
lungnahahd <jyoungjk33@gmail.com>
2021-02-24 10:08:07 +0900

    태그 존재 문장 뒤집기 알고리즘

--- backjoon/switch_str_tag.py
+++ backjoon/switch_str_tag.py
@@ -7,31 +7,38 @@ count = len(senlist)
 finallist = [0 for i in range(count)]
 temp = [0 for i in range(count)]
 wait = False
+give = False
 head = 0
+last = 0
 if count == 1:
     print(sentence)
 else:
     for i in range(count):
         if wait and senlist[i] != ">":
-            finallist[i] = senlist[i]
+            finallist[i] = str(senlist[i])
             continue
         if senlist[i] == "<":
             wait = True
             finallist[i] = senlist[i]
             if head != 0:
-                for j in range(head):
-                    finallist[j] = temp[head - j] 
+                for j in range(head - last):
+                    finallist[i - j - 1] = str(temp[last + j]) 
+                last = head
         elif senlist[i] == ">":
             wait = False
             finallist[i] = senlist[i]
         elif senlist[i] == " ":
-            for j in range(head):
-                finallist[j] = temp[head - j] 
+            finallist[i] = senlist[i]
+            for j in range(head - last):
+                finallist[i -j - 1] = str(temp[last + j])
+            last = head 
         else:
             temp[head] = senlist[i]
             head += 1
-print(finallist)
-#print(''.join(finallist))
+        if i == count - 1 and last != head:
+            for j in range(head - last):
+                finallist[i -j] = str(temp[last + j])
+print(''.join(finallist))
 
 
 
