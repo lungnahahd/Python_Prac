bd9a6f11443a01d470608e83bf66174b128b163c
lungnahahd <jyoungjk33@gmail.com>
2021-03-15 12:12:41 +0900

    에디터 시간 초과 해결

--- backjoon/editer.py
+++ backjoon/editer.py
@@ -1,84 +1,32 @@
 import sys
 input = sys.stdin.readline
 
-import sys
-input = sys.stdin.readline
+getstr = input().strip()
+getstr = list(getstr)
+stack = []
 
-stack1 = input().strip()
-stack1 = list(stack1)
 num = input()
 num = int(num)
 
-stack2 = []
-cursor = len(stack1) - 1
-size = len(stack1) - 1
-
 for i in range(num):
     call = input().strip()
-    if call == "L":
-        if cursor == -1:
+    if call[0] == "L":
+        if len(getstr) == 0:
             continue
-        else:
-            cursor += -1
-    elif call == "D":
-        if cursor == size:
+        stack.append(getstr.pop())
+    elif call[0] == "B":
+        if len(getstr) == 0:
             continue
-        else:
-            cursor += 1
-    elif call == "B":
-        if cursor == -1:
+        getstr.pop()
+    elif call[0] == "D":
+        if len(stack) == 0:
             continue
-        else:
-            stack1.pop(cursor)
-            cursor += -1
+        getstr.append(stack.pop())
     else:
-        call, param = call.split()
-        stack1.insert(cursor + 1 , param)
-        cursor += 1
-
-print(''.join(stack1))
-
+        getstr.append(call[2]) 
+final = "".join(getstr)
+final2 = stack[::-1]
+final2 = "".join(final2)
 
 
-
-
-
-
-
-
-
-
-
-# 시간 초과 발생
-
-# first_str = input().strip()
-# first_arr = list(first_str)
-# num = input()
-# num = int(num)
-# cursor = len(first_arr)
-
-# for i in range(num):
-#     call = input().strip()
-#     if call == "L":
-#         if cursor == 0:
-#             continue
-#         else:
-#             cursor += -1
-#     elif call == "D":
-#         if cursor == len(first_str):
-#             continue
-#         else:
-#             cursor += 1
-#     elif call == "B":
-#         if cursor == 0:
-#             continue
-#         else:
-#             # print(cursor)
-#             del first_arr[cursor -1 ]
-#             cursor += -1
-#     else:
-#         call, param = call.split()
-#         first_arr.insert(cursor , param)
-#         cursor += 1
-        
-# print(''.join(first_arr))
+print(final + final2)
