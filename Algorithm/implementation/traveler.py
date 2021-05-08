feaf1f7ea0ac622ac1dc8e8efc1160746e6dba99
lungnahahd <jyoungjk33@gmail.com>
2021-05-08 19:07:19 +0900

    Fix_traveler.py(1차 구현 완료)

--- Algorithm/implementation/traveler.py
+++ Algorithm/implementation/traveler.py
@@ -8,3 +8,29 @@ move = input().strip()
 move = int(move)
 
 space_array = [[0] * move for _ in range(move)]
+start_l = 1
+start_r = 1
+
+moveplan = input().strip()
+# moveplan 공백 지우기
+moveplan = moveplan.replace(" ","")
+
+for i in range(len(moveplan)):
+    if moveplan[i] == "R":
+        if (start_r + 1) == (move + 1):
+            continue
+        start_r = start_r + 1
+    elif moveplan[i] == "L":
+        if (start_r - 1) == 0:
+            continue
+        start_r = start_r - 1
+    elif moveplan[i] == "U":
+        if (start_l - 1) == 0:
+            continue
+        start_l = start_l - 1
+    elif moveplan[i] == "D":
+        if (start_l + 1) == (move + 1):
+            continue
+        start_l = start_l + 1
+
+print(str(start_l) + " " + str(start_r))
