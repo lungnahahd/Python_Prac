ad9bcf1d6e72c5e5f044dbc7634c8bc9e4058482
lungnahahd <jyoungjk33@gmail.com>
2021-05-15 00:04:58 +0900

    Fix_str_rearrange.py(1차 구현)

--- Algorithm/implementation/str_rearrange.py
+++ Algorithm/implementation/str_rearrange.py
@@ -1,1 +1,19 @@
 # 알파벳과 숫자가 문자열로 주어지면 모든 알파벳은 오름차순으로 정렬, 숫자는 더해서 맨 뒤에 출력
+
+import sys
+input = sys.stdin.readline
+
+give_string = input().strip()
+count = 0
+
+sort = sorted(give_string, reverse=False)
+sort = ''.join(sort)
+
+for i in range(len(sort)):
+    if sort[i].isdigit():
+        count  += int(sort[i])
+    else:
+        result = sort[i:]
+        break
+result = result + str(count)
+print(result)
