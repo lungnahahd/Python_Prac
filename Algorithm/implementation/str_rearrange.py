094d4cb6d03a2800415f1bfb41e290720fa741b5
lungnahahd <jyoungjk33@gmail.com>
2021-05-15 00:11:15 +0900

    Fix_str_rearrange.py(2차 수정본)

--- Algorithm/implementation/str_rearrange.py
+++ Algorithm/implementation/str_rearrange.py
@@ -15,5 +15,27 @@ for i in range(len(sort)):
     else:
         result = sort[i:]
         break
-result = result + str(count)
+if count == 0:
+    result = result
+else:
+    sresult = result + str(count)
 print(result)
+
+# 참고 답안
+# 문자열을 그냥 별도에 리스트에 저장하고 숫자는 내가 하던대로 계산 진행
+# 마지막 출력에 리스트를 정렬하고 숫자의 합을 마지막에 붙이면 정답 가능
+data = input()
+result = []
+value = 0
+# 문자를 하나식 확인
+for x in data:
+    #알파벳인 경우는 리스트에 삽입
+    if x.isalpha():
+        result.append(x)
+    # 숫자인 경우는 따로 더하기
+    else:
+        value += int(x)
+# 알파벳을 오름차순으로 정렬
+result.sort()
+# 숫자가 하나라도 존재하면 가능 뒤에 삽입
+    
