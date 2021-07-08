7e53bfc3e8f1cb955cc1feaa31f5bf08f0fefbcc
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-08 22:56:21 +0900

    Add_selectionsort.py

---
+++ Algorithm/sort/selection_sort.py
@@ -0,0 +1,19 @@
+# 매번 가장 작은 것을 선택하는 선택 정렬
+# 가장 작은 데이터를 선택해 맨 앞에 있는 데이터와 바꾸고, 그 다음 작은 데이터를 선택해서 앞에서 두 번째 데이터와 바꾸는 과정을 반복
+
+# 선택 정렬 연습
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+size = len(array)
+locate = 0
+
+while size > 0:
+    minnum = locate
+    for i in range(size):
+        if minnum != i and array[minnum] > array[i]:
+            minnum = i
+    if minnum != locate:
+        array[minnum],array[locate] = array[locate],array[minnum]
+    size = size -1
+    locate = locate + 1
+print(array)
