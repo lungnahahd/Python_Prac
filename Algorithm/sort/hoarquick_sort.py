43b74a8ac7e9228bdb9a30c8c4c3cf4bb3d591f5
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-17 14:05:23 +0900

    Fix_hoarquicksort(참고답안 추가)

--- Algorithm/sort/hoarquick_sort.py
+++ Algorithm/sort/hoarquick_sort.py
@@ -2,6 +2,9 @@
 # 피벗(기준)을 설정하고 왼쪽에서부터 피벗보다 큰 데이터를 찾고, 오른쪽에서부터 작은 데이터를 찾는다.
 # 왼쪽에서 찾은 큰 데이터와 오른쪽에서 찾은 작은 데이터를 교환하는데 교차가 되는 순간에는 작은 데이터와 피벗과 교환
 # 이전 피벗을 기준으로 위의 과정들을 다시 반복해서 각 리스트가 하나만 남을때까지 반복
+from abc import abstractproperty
+
+
 array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
 
 def quicksort(array, start, end):
@@ -29,3 +32,26 @@ def quicksort(array, start, end):
 
 quicksort(array,0,len(array)-1)
 print(array)
+
+# 참고 답안
+# array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# def quick_sort(array, start, end):
+#     if start >= end: # 정렬 시에 남는 원소가 하나이면 종료
+#         return
+#     pivot = start # 피벗은 start
+#     left = start + 1
+#     right = end
+#     while left <= right:
+#         while left <= end and array[left] <= array[pivot]:
+#             left += 1
+#         while right > start and array[right] >= array[pivot]:
+#             right -= 1
+#         if left > right:
+#             array[right], array[pivot] = array[pivot], array[right]
+#         else:
+#             array[left],array[right] = array[right], array[left]
+#     quick_sort(array,start, right -1)
+#     quick_sort(array,right + 1, end)
+# quick_sort(array,0,len(array)-1)
+# print(array)
