1438a2cb797b8f2c3a9682ba15e1aca2208335f4
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-07-31 01:04:26 +0900

    Fix_RightBigNum(시간 초과 문제)

--- backjoon/right_bignum.py
+++ backjoon/right_bignum.py
@@ -4,31 +4,35 @@
 ### 그 중 가장 대상이랑 가까운, 즉 가장 왼쪽에 있는 수를 선택
 
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 size = int(input())
 
-num_array = []
+
 result = []
 num_list = input()
 num_array = num_list.split()
+# num_array = deque(num_array)
 
 # 한 칸씩 이동시킬 변수
 count = 1
-
 if size != 1:
-    for i in num_array:
-        check = count
-        while check < len(num_array):
-            if int(i) < int(num_array[check]):
-                result.append(int(num_array[check]))
+    for i in range(size):
+        #check = count
+        count = 1
+        while count < len(num_array):
+            if int(num_array[0]) < int(num_array[count]):
+                result.append(int(num_array[count]))
+                del num_array[0]
                 break
             else:
-                if check == (len(num_array) - 1):
+                if count == (len(num_array) - 1):
                     result.append(-1)
+                    del num_array[0]
                     break
-                check += 1
-        count += 1
+                count += 1
+       # count += 1
 result.append(-1)
 
 for i in result:
