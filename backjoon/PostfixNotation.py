34d78c19403e078789bc70d9250f000d2d94d0f0
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-03 23:09:54 +0900

    Fix_PostfixNotation(후위 표기식 2차)

--- backjoon/PostfixNotation.py
+++ backjoon/PostfixNotation.py
@@ -10,8 +10,10 @@ expression = list(expression)
 for i in range(numSize):
     realNum[i] = int(input())
 
+
 # 수식을 알파벳에서 실질적인 수로 변화시키기
-for i in range(numSize*2 -1):
+# 문자열을 입력 받은걸 그대로 리스트로 변환해서 사용하려는 경우, 마지막에 공백(엔터)이 들어가므로 범위 -1 해주기
+for i in range(len(expression) - 1):
     if expression[i] == "+" or expression[i] == "-" or expression[i] == "*" or expression[i] == "/":
         continue
     else:
@@ -20,11 +22,12 @@ for i in range(numSize*2 -1):
 # 피연산자를 잠시 저장할 스택
 tempNum = [] 
 tempNum.append(expression[0])
+
 # 후위표기식 계산
 #result = 0
-for i in range(1,numSize*2 -1):
+# 문자열을 입력 받은걸 그대로 리스트로 변환해서 사용하려는 경우, 마지막에 공백(엔터)이 들어가므로 범위 -1 해주기
+for i in range(1,len(expression) -1 ):
     if expression[i] == "+" or expression[i] == "-" or expression[i] == "*" or expression[i] == "/":
-        print(i)
         b = tempNum.pop()
         a = tempNum.pop()
         if expression[i] == "+":
@@ -37,7 +40,7 @@ for i in range(1,numSize*2 -1):
             c = a/b
     else:
         c = expression[i]
-    if i < numSize*2-2:
+    if i < len(expression) - 2:
         tempNum.append(c)
     else:
         result = c
