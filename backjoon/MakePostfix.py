fe008c94996d76b2e5d2a0bcf6fe4123ad485689
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-03 23:12:03 +0900

    Fix_MakePostfix(후위표기식 2차)

--- backjoon/MakePostfix.py
+++ backjoon/MakePostfix.py
@@ -1,7 +1,8 @@
- 후위 표기식으로 변환
+ #후위 표기식으로 변환
 ## 식이 주어지면 이를 후위표기식으로 바꾸는 것
 ### 입력은 단순히 식 하나
 #### 
+
 
 import sys
 input = sys.stdin.readline
@@ -12,15 +13,21 @@ expression = expression[:-1]
 
 tempSave = []
 priSave = []
+next = False
 result = ""
 priCount = 0
 for i in range(len(expression)):
     if expression[i].isupper():
         result += expression[i]
+        if next:
+            result += tempSave.pop()
+            next = False
     elif expression[i] == "+" or expression[i] == "-" or expression[i] == "*" or expression[i] == "/":
         if priCount != 0:
             priSave.append(expression[i])
         else:
+            if expression[i] == "*" or expression[i] == "/":
+                next = True
             tempSave.append(expression[i])
     elif expression[i] == "(":
         priCount += 1
