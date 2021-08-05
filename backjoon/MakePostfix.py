c4f14aab625cf1ee093a1e0fdae6e3388aeea90b
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-05 23:19:59 +0900

    Fix_MakePostfix(후위표현식 4차 구현)

--- backjoon/MakePostfix.py
+++ backjoon/MakePostfix.py
@@ -10,29 +10,35 @@ input = sys.stdin.readline
 expression = input()
 expression = list(expression)
 expression = expression[:-1]
-
-tempSave = [] # 임시적으로 연산자를 저장하는 배열 
-priSave = []  # 괄호 나왔을 때, 저장하는 우선순위 높은 배열
-
-priResult = ""
-
-priCount = 0  # 괄호의 열고 닫음을 나타내주는 변수
-progress = 0  # 현재 진행상황을 알려주는 변수
-result = ""   # 결과를 담는 변수
-count = 0
+# 중위 표현식에서 우선순위를 나타내는 딕셔너리
+priArray = {'(' : 0, ')' : -1, '*' : 1, '/' : 1, '+' : 2, '-' : 2}
+# 결과를 담는 변수
+result = ""
+# 연산자를 임시적으로 담는 변수
+tempSave = []
+# 현재 진행 상황을 나타내는 변수
+progress = 0
 
 while progress < len(expression):
-    if expression[progress] == "(":
-        while expression[progress + count] != ")":
-            priResult
+    if expression[progress].isupper():
+        result += expression[progress]
+    else:
+        if expression[progress+1].isupper():
+            if tempSave:
+                if priArray[tempSave[-1]] >= priArray[expression[progress]]:
+                    result += expression[progress + 1]
+                    result += expression[progress]
+                    progress += 1
+                else:
+                    result += expression[progress + 1]
+                    result += tempSave.pop()
+                    tempSave.append(expression[progress])
+            else:
+                tempSave.append(expression[progress])        
+    progress += 1
 
+if tempSave:
+    while len(tempSave) != 0:
+        result += tempSave.pop()
 
-    elif expression[progress] == "*" or expression[progress] == "/":
-        if expression[progress+1] == "(":
-            priSave.append(expression[progress])
-        else:
-            result += expression[progress+1]
-            result += expression[progress]
-            progress += 2
-    elif expression[progress] == "+" or expression[progress] == "-":
-        
+print(result)
