e158abfd3c1765d8def073b3f6e74c1365407c02
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-09 23:15:01 +0900

    Fix_GDLM(최대공약수, 최소공배수 2차 구현)

--- backjoon/GreatestDivisionLeastMultiple.py
+++ backjoon/GreatestDivisionLeastMultiple.py
@@ -5,7 +5,7 @@
 
 import sys
 sys.stdin.readline
-
+# 입력 받은 두 수를 큰 것과 작은 것으로 구분하기
 big = 0
 small = 0
 
@@ -20,33 +20,33 @@ else:
     big = b
     small = a
 
-GD = False
-GDNum = 0
-check = small
-
+GD = False # 최대공약수 찾기 성공 여부를 알려주는 변수
+GDNum = 0 # 최대공약수를 담는 변수
+check = small # 최대공약수를 찾는 기준이 되는 변수 -> 해당 변수를 바꾸어가면서 최대공약수를 찾아나갈 것
+# 최대공약수를 구하는 부분
 while not GD:
-    if small % check == 0:
+    if small % check == 0: 
         if big % check == 0:
-            GD = True
-            GDNum = check
+            GD = True # 최대공약수를 찾으면 반복문 종료
+            GDNum = check # 찾은 최대공약수를 담기
         else:
-            check -= 1
+            check -= 1 # 해당 수가 최대공약수가 아니라면 행하는 부분
     else:
-        check -= 1
-    if check == 0:
+        check -= 1 # 해당 수가 최대공약수가 아니라면 행하는 부분
+    if check == 0: # 끝까지 찾아는데 없으면 그냥 반복문 탈출
         break
 
-LM = False
-LMNum = 0
-check = 1
-
-while not LM:
-    big = big * check
-    if big % small == 0:
-        LM = True
-        LMNum = big
+LM = False # 최소공배수 찾기 성공 여부를 알려주는 변수
+LMNum = 0 # 최소공배수를 담는 변수
+check = 1 # 최소공배수를 찾기 위해 곱하기를 해주는 변수 -> 점점 증가시켜서 최소공배수를 찾아나가기
+# 최소공배수를 구하는 부분
+while not LM:  
+    increaseNum = big * check # 최소공배수를 찾는 실질적 변수
+    if increaseNum % small == 0:
+        LM = True # 최소공배수를 찾았을 경우 반복문 탈출
+        LMNum = increaseNum # 최소공배수를 담는 변수
     else:
-        check += 1
-
+        check += 1 # 최소공배수가 아닐 경우 점점 수를 늘려가면서 최소공배수 찾기
+# 결과를 출력
 print(GDNum)
 print(LMNum)
