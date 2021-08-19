db222bb8236febfd3ce48de4451c9a1597b20029
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-20 00:25:04 +0900

    Feat : "조합 0의 개수" 3차 구현

--- backjoon/ZeroCombinationCount.py
+++ backjoon/ZeroCombinationCount.py
@@ -2,6 +2,10 @@
 ## 조합 nCm의 끝자리 0의 갯수를 출력하는 프로그램
 ### 입력 : 첫 줄에 정수 n,m 2개 입력
 ### 출력 : 첫 줄에 nCm의 끝자리 0의 갯수를 출력 
+#### 처음에는 팩토리얼로 처리를 진행했는데, 팩토리어을 이용하면 입력 수가 커졌을 때, 컴파일 에러 발생
+#### 단순하게 0의 갯수만 구하면 되므로 그것만 구하도록 유도하기!
+#### 조합 : (N!) / ((N-M)! * (M!))
+
 
 import sys
 input = sys.stdin.readline
@@ -27,5 +31,6 @@ def findFive(num):
     return count
 
 # 조합의 0을 계산하는 부분
-result = min(findFive(N),findTwo(N)) - min(findTwo(M)+findTwo(N-M),findFive(M)+findFive(N-M))
+# 분자, 분모를 구분해서 계산하지 말고, 먼저 2 따로 5 따로 각각 전체 계산 후, 결과 도출!
+result = min(findTwo(N) - findTwo(N-M) -findTwo(M), findFive(N) - findFive(N-M) - findFive(M))
 print(result)
