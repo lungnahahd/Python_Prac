5c2ef6081c37a4df4935228dd0a02ab0f268b680
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-02 08:47:32 +0900

    Feat  : "2xn 타일링 구현" 시간 초과 발생

--- backjoon/DynamicPrograming/2xnTiling.py
+++ backjoon/DynamicPrograming/2xnTiling.py
@@ -10,13 +10,13 @@ n = int(input()) # n을 입력 받는 부분
 count = 1 # 결과를 count하는 부분으로 모두 1로 채우는 경우는 항상 존재하므로 1부터 시작
 
 def CountCheck(num):
-    global count
-    semiCount = 0
     check = 0
-    if num <= 2:
-        return 1 
-    elif num > 2:
-        check += num - 2
-        return check * CountCheck(num - 2)
+    if num < 2:
+        return 0 
+    elif num >= 2:
+        check += num - 1
+        for i in range(num-1):
+            check += CountCheck(num -2 -i)
+        return check
 
-print(CountCheck(n))
+print((CountCheck(n) + 1) % 10007)
