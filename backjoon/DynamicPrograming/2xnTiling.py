7599462e7df9ab70ce38394807dc071badebb19c
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-02 09:19:19 +0900

    Fix : "시간초과 문제 해결"

--- backjoon/DynamicPrograming/2xnTiling.py
+++ backjoon/DynamicPrograming/2xnTiling.py
@@ -9,14 +9,11 @@ input = sys.stdin.readline
 n = int(input()) # n을 입력 받는 부분
 count = 1 # 결과를 count하는 부분으로 모두 1로 채우는 경우는 항상 존재하므로 1부터 시작
 
-def CountCheck(num):
-    check = 0
-    if num < 2:
-        return 0 
-    elif num >= 2:
-        check += num - 1
-        for i in range(num-1):
-            check += CountCheck(num -2 -i)
-        return check
+countList = [0 for i in range(1001)] # dp 프로그래밍을 위해서 빈 배열 생성
+countList[1] = 1
+countList[2] = 2
 
-print((CountCheck(n) + 1) % 10007)
+for i in range(3,n+1):
+    countList[i] = countList[i-1] + countList[i - 2]
+
+print(countList[n] % 10007)
