c647787ce17c0c740654c0223d5ea59879447557
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-07 11:35:56 +0900

    Feat : "Plus123 구현" 연속된 수 불가한 응용 구현

---
+++ backjoon/DynamicPrograming/Plus123_5.py
@@ -0,0 +1,30 @@
+# 1,2,3 더하기 5
+## 주어진 정수를 1,2,3 합으로 나타내는 방법의 수를 구하기
+## 단, 같은 수를 두 번 이상 연속 사용해서는 안됨
+### 입력 : 첫 줄에 테스트 개수가 입력되고, 그 이후에 차례로 구할 정수가 입력
+### 출력 : 각 테스트 마다 결과를 한 줄에 출력
+
+import sys
+input = sys.stdin.readline
+
+caseCount = int(input())
+#countList = [0 for i in range(100001)]
+countList = [[0 for col in range(4)] for row in range(100001)]
+countList[1][1] = 1
+countList[2][2] = 1
+countList[3][1] = 1
+countList[3][2] = 1
+countList[3][3] = 1
+
+for i in range(4,100001):
+    countList[i][1] = countList[i-1][3] + countList[i-1][2]
+    countList[i][2] = countList[i-2][1] + countList[i-2][3]
+    countList[i][3] = countList[i-3][1] + countList[i-3][2]
+
+result = []
+for i in range(caseCount):
+    num = int(input())
+    result.append((countList[num][1] + countList[num][2] + countList[num][3])%1000000009)
+
+for i in result:
+    print(i)
