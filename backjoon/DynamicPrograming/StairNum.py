e3af37d8fad41e60213fc0c60a3d702efe9074ba
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-10 11:04:46 +0900

    Fix : "쉬운 계단수 에러 해결"

--- backjoon/DynamicPrograming/StairNum.py
+++ backjoon/DynamicPrograming/StairNum.py
@@ -9,10 +9,22 @@ N = int(input()) # 자리수 입력 받기
 
 MAX = 1000000000 # 최종으로 나누어 줄 수
 
-starNumList = [0 for i in range(101)]
+starNumList = [[0] * 10 for i in range(N+1)] # 각 자릿수의 끝자리 수의 개수를 체크해서 작성될 배열
 
-starNumList[1] = 9
-for i in range(2,101):
-    starNumList[i] = ((starNumList[i-1] * 2) - 1) % MAX
+# 초기에 한자리 수는 0이 안들어가는 특별한 경우 이므로 초기 설정으로 처리 해주기
+for i in range(1,10):
+    starNumList[1][i] = 1
 
-print(starNumList[N])
+
+for col in range(2, N+1):
+    for row in range(10):
+        if row == 0:
+            starNumList[col][row] = starNumList[col-1][1] # 0일 경우는 이전 자리수가 1인 경우만 올 수 있음
+        elif row == 9: 
+            starNumList[col][row] = starNumList[col-1][8] # 9일 경우는 이전 자리수가 8인 경우만 올 수 있음
+        else:
+            starNumList[col][row] = starNumList[col-1][row-1] + starNumList[col-1][row+1] # 1~8인 경우는 이전에 일을 빼고 일을 더한 경우에 가능
+result = 0
+for i in range(10):
+    result += starNumList[N][i]
+print(result % MAX) 
