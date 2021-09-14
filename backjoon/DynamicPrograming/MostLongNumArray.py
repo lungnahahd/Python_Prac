d857a0de30569e2b4036c2a13491110ea1630bd6
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-14 09:33:08 +0900

    Fix  : "반례 수정 중"

--- backjoon/DynamicPrograming/MostLongNumArray.py
+++ backjoon/DynamicPrograming/MostLongNumArray.py
@@ -8,16 +8,25 @@ input = sys.stdin.readline
 
 size = int(input()) # 수열의 크기
 numList = input().split() # 수열을 입력
+count = [1 for i in range(size)] # 각 수열의 크기를 카운트 해줄 리스트
 
-result = 0
-for i in range(size):
-    count = 1
-    bigNum = int(numList[i])
-    for j in range(i + 1,size):
-        if bigNum < int(numList[j]):
-            bigNum = int(numList[j])
-            count += 1
-    if count > result:
-        result = count
-
+result = 1 # 최종 크기를 나타낼 변수
+for i in range(1,size):
+    nowWhere = size - i
+    move = nowWhere
+    while move != 0:
+        if numList[nowWhere] > numList[move-1]:
+            if count[nowWhere] < count[move-1]:
+                move -= 1
+                continue
+            count[move-1] = count[nowWhere] + 1
+            if count[move-1] > result:
+                result = count[move-1]
+            move -= 1
+        else:
+            move -= 1
 print(result)
+print(count)
+
+
+# 1 5 10 3 13 18 15 16 이 왜 6이 안되는가?
