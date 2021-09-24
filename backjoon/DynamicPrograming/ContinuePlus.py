94c582c9351267f31a1ad2b582297bdac57ae440
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-24 11:23:11 +0900

    Fix : "연속합" 3차 구현

--- backjoon/DynamicPrograming/ContinuePlus.py
+++ backjoon/DynamicPrograming/ContinuePlus.py
@@ -3,6 +3,16 @@
 ### 입력 : 첫 줄에 정수의 크기가 입력되고, 두번 째 줄에는 수열이 입력
 ### 출력 : 첫 줄에 연속 수열 합의 최대가 출력
 
+
+################### 같은 부호일 경우 tempResult에서 동일하게 처리해줘보기
+
+# 10
+# 14 40 80 84 -22 -28 72 80 69 -44
+# 221
+
+# 정답 : 390
+
+
 import sys
 from typing import Counter, final
 input = sys.stdin.readline
@@ -23,32 +33,54 @@ for i in numList:
             tempResult.append(temp)
             temp = 0
             tempResult.append(i)
-    elif i >= 0:
+    elif i > 0:
         temp += i
-#print(tempResult)
+    elif i == 0:
+        tempResult.append(i)
 tempLarge = max(tempResult)
+
+plusTemp = 0
+minusTemp = 0
+secondTemp = []
+for i in tempResult:
+    if i < 0:
+        minusTemp += i
+        if plusTemp != 0:
+            secondTemp.append(plusTemp)
+            plusTemp = 0
+    elif i > 0:
+        plusTemp += i
+        if minusTemp != 0:
+            secondTemp.append(minusTemp)
+            minusTemp = 0
+if minusTemp != 0:
+    secondTemp.append(minusTemp)
+if plusTemp != 0:
+    secondTemp.append(plusTemp)
+            
 
 temp = 0
 finalResult = []
-count = 0
-for i in range(len(tempResult)):   
-    if tempResult[i] < 0 :
+for i in range(len(secondTemp)):   
+    if secondTemp[i] < 0 :
         if i == 0:
-            finalResult.append(tempResult[i])
-        elif i == len(tempResult) - 1:
-            finalResult.append(tempResult[i-1])
-            finalResult.append(tempResult[i])
+            continue
+            #finalResult.append(tempResult[i])
+        elif i == len(secondTemp) - 1:
+            break
         else:
-            if -tempResult[i] < tempResult[i-1] and -tempResult[i] < tempResult[i+1] and tempResult[i-1] > 0 and tempResult[i + 1] > 0:
-                finalResult.append(tempResult[i]+tempResult[i-1] + tempResult[i+1])
+            if -secondTemp[i] < secondTemp[i-1] and -secondTemp[i] < secondTemp[i+1]:
+                finalResult.append(secondTemp[i] + secondTemp[i-1] + secondTemp[i + 1])
             else:
-                finalResult.append(tempResult[i-1])
+                finalResult.append(secondTemp[i-1])
     elif tempResult[i] > 0 and i == len(tempResult) -1 :
         finalResult.append(tempResult[i])
-finalLarge = max(finalResult)
-
-if finalLarge > tempLarge:
-    print(finalLarge)
-else:
+if len(finalResult) == 0:
     print(tempLarge)
-    
+else:
+    finalLarge = max(finalResult)
+
+    if finalLarge > tempLarge:
+        print(finalLarge)
+    else:
+        print(tempLarge)
