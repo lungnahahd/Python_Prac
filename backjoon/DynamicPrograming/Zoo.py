6c8cb790f855bba598aed467fbb1a6a6fd2044b6
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-11-12 15:22:29 +0900

    Feat  : "동물원" 1차 구현

--- backjoon/DynamicPrograming/Zoo.py
+++ backjoon/DynamicPrograming/Zoo.py
@@ -2,8 +2,21 @@
 ## 동물원에 가로로 두칸 세로로 N 칸인 우리
 ## 우리에 사자를 배치할 때, 가로나 세로에서 붙어있게 배치를 못 할때, 사자를 배치하는 경우의 수 구하기
 ### 입력 : 첫 줄에 우리의 크기 N 을 입력
-### 출력 : 첫 줄에 사자를 배치하는 경우의 수를 출력
+### 출력 : 첫 줄에 사자를 배치하는 경우의 수를 9901로 나눈 나머지를 출력
 
 import sys  
 input = sys.stdin.readline
 
+division = 9901
+
+zooSize = int(input())
+ 
+cage = [0 for i in range(100001)]
+cage[1] = 1
+cage[2] = 7
+cage[3] = 17
+
+for i in range(4,100001):
+    cage[i] = (cage[i-1] * 2)%division + cage[i-2] % division
+
+print(cage[zooSize] % division)
