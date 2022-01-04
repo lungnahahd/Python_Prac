ccb06e07fedb85754a73211a9027905a5144d5c3
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-01-04 14:17:49 +0900

    Feat : "3차 구현"

--- backjoon/DynamicPrograming/Drink.py
+++ backjoon/DynamicPrograming/Drink.py
@@ -12,41 +12,31 @@ input = sys.stdin.readline
 drinkNum = int(input())
 
 drinkCost = [0 for i in range(drinkNum)]
-nowDrink = [[] for i in range(drinkNum)]
-
+drinkArr = [[0,0,0] for i in range(drinkNum)]
 large = 0
+
 # 포도주 술 양 받기
 for i in range(drinkNum):
     get= int(input())
     drinkCost[i] = get
-    nowDrink[i].append([drinkCost[i],1])
-    
+if drinkNum >= 3:
+    drinkArr[0][0] = drinkCost[0]
+    drinkArr[1][0] = drinkCost[1]
+    drinkArr[1][1] = drinkCost[1] + drinkCost[0]
+    drinkArr[2][0] = drinkCost[2] + drinkCost[0]
+    drinkArr[2][1] = drinkCost[2] + drinkCost[1]
+    drinkArr[2][2] = drinkArr[1][1]
+    if drinkNum > 3:
+        for i in range(3,drinkNum):
+            drinkArr[i][0] = drinkArr[i-2][1] + drinkCost[i]
+            drinkArr[i][1] = drinkArr[i-1][0] + drinkCost[i]
+            drinkArr[i][2] = drinkArr[i-1][1]
+else:
+    if drinkNum == 1:
+        drinkArr[0][0] = drinkCost[0]
+    else:
+        drinkArr[1][0] = drinkCost[0] + drinkCost[1]
 
-# print(nowDrink[0][0][0]) # 포도주 자체의 양을 카운트
-# print(nowDrink[0][0][1]) # 포도주 연속 마신 것 카운트 
 
 
-# 포도주 마시는 것 카운트
-for i in range(1,drinkNum):
-    for a in range(len(nowDrink[i-1])):
-        if nowDrink[i-1][a][1] == 2:
-            nowDrink[i].append([nowDrink[i-1][a][0],0])
-            if large < nowDrink[i-1][a][0]:
-                large = nowDrink[i-1][a][0]
-        else:
-            nowDrink[i].append([drinkCost[i]+nowDrink[i-1][a][0],nowDrink[i-1][a][1]+1])
-            if drinkCost[i]+nowDrink[i-1][a][0] > large:
-                large = drinkCost[i]+nowDrink[i-1][a][0]
-            nowDrink[i].append([nowDrink[i-1][a][0],0])
-            if nowDrink[i-1][a][0] > large:
-                large = nowDrink[i-1][a][0]
-
-# 만들어진 포도주 배열에서 최대를 선택해서 출력
-# large = 0
-# for i in range(len(nowDrink[drinkNum-1])):
-#     if large < nowDrink[drinkNum-1][i][0]:
-#         large = nowDrink[drinkNum-1][i][0]
-print(large)
-
-# print(nowDrink[drinkNum-1])
-
+resultDrink = print(max(drinkArr[drinkNum-1]))
