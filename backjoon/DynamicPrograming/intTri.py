faf2a0a10b8f607c8993ae9c694d1bc386294d93
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-01-06 16:24:15 +0900

    Feat : "정수 삼각형" 1차 구현

--- backjoon/DynamicPrograming/intTri.py
+++ backjoon/DynamicPrograming/intTri.py
@@ -7,3 +7,25 @@
 import sys
 input = sys.stdin.readline
 
+stair = int(input())
+costStair = [[] for i in range(stair)]
+
+# 정수 삼각형의 값 입력 받기
+for i in range(stair):
+    get = input().split()
+    costStair[i] = get
+
+result = [[] for i in range(stair)] # 결과를 담을 배열
+result[0].append(int(costStair[0][0]))
+
+# 경우를 나누어서 결과 배열을 생성
+for i in range(1, stair):
+    for j in range(len(costStair[i])):
+        if j == 0:
+            result[i].append(result[i-1][j] + int(costStair[i][j]))
+        elif j == len(costStair[i])-1:
+            result[i].append(result[i-1][j-1] + int(costStair[i][j]))
+        else:
+            result[i].append(max(result[i-1][j-1], result[i-1][j]) + int(costStair[i][j]))
+# 마지막 결과 배열 중 최대를 출력
+print(max(result[stair-1]))
