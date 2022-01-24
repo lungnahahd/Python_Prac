37f412d23de7e8cd6c741a534a96e74222afb354
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-01-24 22:53:43 +0900

    Feat : "가장 긴 바이토닉 부분 수열" 2차 구현

--- backjoon/DynamicPrograming/Bytonic.py
+++ backjoon/DynamicPrograming/Bytonic.py
@@ -3,70 +3,36 @@
 ### 입력 : 첫 줄의 수열의 길이와 두번 째 줄에 수열을 입력
 ### 출력 : 주어진 수열의 가장 긴 바이토닉 부분 수열의 길이를 출력
 
-from gettext import install
 import sys
 input = sys.stdin.readline
 
 size = int(input())
 list = input().split()
-reverseList = list
-reverseList.reverse()
+reverseList = list[:] # 파이썬 리스트를 똑같이 복사 -> 이렇게 안 하고 대입하면 주소가 복사!
+reverseList.reverse() # 복사한 리스트를 순서 뒤집기
 
-increase = [1 for i in range(size)]
-reIncrease = [1 for i in range(size)]
+increase = [1 for i in range(size)] # 원래 순서대로 진행하는 카운트
+reIncrease = [1 for i in range(size)] # 뒤로 진행하는 카운트
 
-result = [0 for i in range(size)]
+result = [0 for i in range(size)] # 결과를 담을 배열
 
-# 각 위치에서 증가하는 배열의 길이 구하기
+# 앞에서 증가하는 것, 거꾸로 증가하는 것 카운트
 for i in range(1,size):
-    big = 0
-    reverse = 0
+    big = 0 # 각 경우에 가장 큰 카운트를 담을 변수
+    reverseBig = 0 # 뒤집어서 각 경우 처리
     for j in range(i):
         if int(list[i]) > int(list[j]):
             if big < increase[j]:
                 big = increase[j]
         if int(reverseList[i]) > int(reverseList[j]):
-            if reverse < reIncrease[j]:
-                reverse = reIncrease[j]
-    increase[i] += big
-    reIncrease[i] += reverse
+            if reverseBig < reIncrease[j]:
+                reverseBig = reIncrease[j]
+    increase[i] = big + 1
+    reIncrease[i] = reverseBig + 1
 reIncrease.reverse()
 
-print(reIncrease[8:])
-print(increase[:7])
-
 for i in range(size):
-    if i != 0 and i != size-1:
-        front = increase[:i]
-        back = reIncrease[i+1:]
-        big = 0
-        small = 0
-        for j in range(i):
-            if int(list[i]) > int(list[j]):
-                if big < front[j]:
-                    big = front[j]
-        for k in range(len(back)):
-            if int(list[i]) > int(list[k+i+1]):
-                if small < back[k]:
-                    small = back[k]
-        print(big)
-        print(small)
-        result[i] = big + small + 1
+    result[i] = reIncrease[i] + increase[i] - 1
 
-# for i in range(1,size):
-#     if int(list[0]) > int(list[i]):
-#         if result[0] < increase[i]:
-#             result[0] = increase[i] + 1
+print(max(result))
 
-
-# for i in range(size-1):
-#     big =0
-#     if int(list[size-1]) > int(list[i]):
-#         if result[size-1] < reIncrease[i]:
-#             result[size-1] = reIncrease[i] + 1
-
-
-
-# print(increase)
-# print(reIncrease)
-# print(result)
