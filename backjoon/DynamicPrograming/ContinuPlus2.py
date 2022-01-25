81562b057a924eed5a918ceb7fadd1271805333e
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-01-25 10:55:01 +0900

    Feat  : "연속합2" 1차 구현

--- backjoon/DynamicPrograming/ContinuPlus2.py
+++ backjoon/DynamicPrograming/ContinuPlus2.py
@@ -8,3 +8,25 @@ import sys
 input = sys.stdin.readline
 
 size = int(input())
+list = input().split()
+sumList = []
+tempSum = 0
+for i in range(size):
+    if int(list[i]) < 0 :
+        sumList.append(tempSum)
+        tempSum = 0
+        sumList.append(int(list[i]))
+    else :
+        tempSum += int(list[i])
+oneResult = [0 for i in range(len(sumList))]
+twoResult = [0 for i in range(len(sumList))]
+oneResult[0] = sumList[0]
+case1 = max(sumList)
+
+for i in range(1,len(sumList)):
+    oneResult[i] = oneResult[i-1] + sumList[i]
+    twoResult[i] = max(oneResult[i-1], twoResult[i-1] + sumList[i])
+case2 = max(oneResult)
+case3 = max(twoResult)
+
+print(max(case1,case2,case3))
