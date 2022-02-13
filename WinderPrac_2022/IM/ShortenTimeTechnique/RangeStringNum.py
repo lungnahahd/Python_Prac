1227d933bad39e395bb37def653eeae37efbdcde
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-14 02:50:19 +0900

    Fix : "반례 해결" 2차 구현

--- WinderPrac_2022/IM/ShortenTimeTechnique/RangeStringNum.py
+++ WinderPrac_2022/IM/ShortenTimeTechnique/RangeStringNum.py
@@ -14,3 +14,25 @@ for i in range(n):
     get = list(input())
     stringArr.append(get)
 
+
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if stringArr[i-1][j-1] == "a":
+            prefix[i][j][0] = prefix[i-1][j][0] + prefix[i][j-1][0] - prefix[i-1][j-1][0] + 1
+            prefix[i][j][1] = prefix[i-1][j][1] + prefix[i][j-1][1] - prefix[i-1][j-1][1]
+            prefix[i][j][2] = prefix[i-1][j][2] + prefix[i][j-1][2] - prefix[i-1][j-1][2]
+        elif stringArr[i-1][j-1] == "b":
+            prefix[i][j][1] = prefix[i-1][j][1] + prefix[i][j-1][1] - prefix[i-1][j-1][1] + 1
+            prefix[i][j][0] = prefix[i-1][j][0] + prefix[i][j-1][0] - prefix[i-1][j-1][0]
+            prefix[i][j][2] = prefix[i-1][j][2] + prefix[i][j-1][2] - prefix[i-1][j-1][2]
+        else:
+            prefix[i][j][2] = prefix[i-1][j][2] + prefix[i][j-1][2] - prefix[i-1][j-1][2] + 1
+            prefix[i][j][0] = prefix[i-1][j][0] + prefix[i][j-1][0] - prefix[i-1][j-1][0]
+            prefix[i][j][1] = prefix[i-1][j][1] + prefix[i][j-1][1] - prefix[i-1][j-1][1]
+
+for i in range(k):
+    r1, c1, r2, c2 = input().split()
+    r1, c1, r2, c2 = int(r1), int(c1), int(r2), int(c2)
+    print(prefix[r2][c2][0]-prefix[r1-1][c2][0]-prefix[r2][c1-1][0] + prefix[r1-1][c1-1][0] , end=' ')
+    print(prefix[r2][c2][1]-prefix[r1-1][c2][1]-prefix[r2][c1-1][1] + prefix[r1-1][c1-1][1], end=' ')
+    print(prefix[r2][c2][2]-prefix[r1-1][c2][2]-prefix[r2][c1-1][2] + prefix[r1-1][c1-1][2])
