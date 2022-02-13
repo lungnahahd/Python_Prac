acd9603f09a02d04be9fa5e632b682041e8cf717
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-02-13 20:51:39 +0900

    Feat : "부분 수열의 합이 k" 1차 구현

---
+++ WinderPrac_2022/IM/ShortenTimeTechnique/PartNumSumK.py
@@ -0,0 +1,21 @@
+# 부분 수열의 합이 K
+## n개의 정수로 이루어진 수열에서 연속된 구간의 합을 구하려합니다.
+## 모든 연속된 구간의 합 중에서 합이 k인 것의 개수를 구하는 프로그램을 작성해보세요.
+
+cmd = list(map(int, input().split()))
+
+numList = list(map(int,input().split()))
+prefix = [0]
+
+sumNum = 0
+for i in numList:
+    sumNum += i
+    prefix.append(sumNum)
+
+count = 0
+if prefix[2] == cmd[1]:
+    count += 1
+for i in range(3,cmd[0]+1):
+    if cmd[1] == (prefix[i] - prefix[i-2]):
+        count += 1
+print(count)
