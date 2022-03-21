07cee79cc3e0c75d8b1770af5a82ab9fb00e1990
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-03-21 17:12:46 +0900

    Add : "N과 M(2)" 1차 구현

---
+++ backjoon/BruteForce/N&M2.py
@@ -0,0 +1,37 @@
+# N과 M(2)
+## 1 부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열(수열은 오름 차순)
+### 입력 : 첫 줄에 자연수 N과 M이 입력
+### 출력 : 첫 줄에 수열을 출력
+
+import sys
+input = sys.stdin.readline
+
+numLarge, numCount = tuple(map(int,input().split()))
+
+check = [False for _ in range(numLarge + 1)]
+finNum = [False for _ in range(numLarge + 1)]
+save = []
+
+
+def FindNumArr(count):
+    if count == numCount:
+        #save.sort()
+        for k in save:
+            #if temp == -1:
+                #finNum[k] = True
+                #temp = 1
+            print(k,end=' ')
+        print()
+        return
+    
+    for i in range(1, numLarge+1):
+        if not check[i]:
+            if len(save) != 0 and save[-1] > i:
+                continue
+            check[i] = True
+            save.append(i)
+            FindNumArr(count+1)
+            save.pop()
+            check[i] = False
+
+FindNumArr(0)
