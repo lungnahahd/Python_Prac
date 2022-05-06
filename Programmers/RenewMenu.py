281c4ac6211683b54c6c238a4461dbee231325ad
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-05-07 00:37:33 +0900

    Add : "메뉴 리뉴얼" 2차 구현

--- Programmers/RenewMenu.py
+++ Programmers/RenewMenu.py
@@ -1,20 +1,51 @@
 # 메뉴 리뉴얼
 
+import heapq
+
 def solution(orders, course):
     answer = []
-    nowSize = course[0]
-    nowMenu = 0
-    checkWord = [[] for _ in range(26)]
-    for j in range(len(orders)): # 26개의 메뉴판에 한 명씩 자신의 메뉴를 체크해서 넣어놓는 과정
-        for i in range(len(orders[j])):
-            temp = ord(orders[j][i]) - 65
-            checkWord[temp].append(j)
-        
-    for i in range(26):    
-        
-    
-    
-    
-    
-    
+    orders.sort(key=len) # 리스트 요소를 길이 순으로 정렬
+    countResult = {}
+    print(orders)
+    for i in range(len(orders)):
+        for j in range(i+1,len(orders)):
+            temp = ""
+            for k in orders[i]:
+                if k in orders[j]:
+                    temp += k
+            if len(temp) in course:
+                temp = list(temp)
+                temp.sort()
+                temp = ''.join(temp)
+                if temp in countResult:
+                    countResult[temp] += 1
+                else:
+                    countResult[temp] = 1
+    now = list(countResult.items())
+    q = []
+    for i in now:
+        heapq.heappush(q,(-len(i[0]),-i[1],i[0]))
+    nowCount = 0
+    beforeMax = 0
+    print(q)
+    while len(q) != 0:
+        a = q[0]
+        heapq.heappop(q)
+        if -a[0] == nowCount:
+            if beforeMax <= -a[1]:
+                answer.append(a[2])
+        else:
+            nowCount = -a[0]
+            beforeMax = -a[1]
+            answer.append(a[2])
+
+
+
+    answer = list(set(answer))
+    answer.sort()
     return answer
+
+a = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+b = [2,3,5]
+
+print(solution(a,b))
