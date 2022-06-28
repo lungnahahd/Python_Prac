88d5927d17ad701dd290432c5adab2f86b02191d
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-06-29 00:25:46 +0900

    Fix : "네 방향 탈출 가능 여부 판별" 2차 구현_반례 생성

--- Algorithm/dfsbfs/FourWay_Snake.py
+++ Algorithm/dfsbfs/FourWay_Snake.py
@@ -1,5 +1,8 @@
 # 네 방향 탈출 가능 여부 판별
 
+from collections import deque
+
+
 n,m = list(map(int,input().split()))
 showMap = []
 
@@ -13,17 +16,37 @@ findWay = 0
 check = [[False for _ in range(m)] for _ in range(n)]
 
 def bfs(row, col, check):
-    global findWay
-    if 0 <= row < n and 0 <= col < m:
-        if row == n-1 and col == m-1:
-            findWay = 1
-        else:
-            if not check[row][col] and showMap[row][col] == "1":
-                check[row][col] = True
-                bfs(row+1,col,check)
-                bfs(row-1,col,check)
-                bfs(row,col+1,check)
-                bfs(row,col-1,check)
+    check[row][col] = True
+    findWay = 0
+    if showMap[row][col] == "1":
+        queue = deque([(row,col)])
+        while queue:
+            rNow, cNow = queue.popleft()
+            #check[rNow][cNow] = True
+            if rNow == n-1 and cNow == n-1:
+               findWay = 1
+               break 
+            else:
+                if 0 <= rNow-1:
+                    if not check[rNow-1][cNow] and showMap[rNow-1][cNow] == "1":
+                        queue.append((rNow-1,cNow))
+                        check[rNow-1][cNow] = True
+                if rNow +1 < n:
+                    if not check[rNow+1][cNow] and showMap[rNow+1][cNow] == "1":
+                        queue.append((rNow+1,cNow))
+                        check[rNow+1][cNow] = True
+                if 0 <= cNow -1:
+                    if not check[rNow][cNow-1] and showMap[rNow][cNow-1] == "1":
+                        queue.append((rNow, cNow-1))
+                        check[rNow][cNow-1] = True
+                if cNow+1 < m:
+                    if not check[rNow][cNow+1] and showMap[rNow][cNow+1] == "1":
+                        queue.append((rNow,cNow+1))
+                        check[rNow][cNow+1] = True
+                        
+            
+
     
     return findWay
+    
 print(bfs(0,0,check))
