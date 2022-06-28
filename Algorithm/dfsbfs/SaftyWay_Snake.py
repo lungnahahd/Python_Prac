abfb84e1517aa9f3e11363c6e06d5187a3002399
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-06-28 19:29:29 +0900

    Fix : "두 방향 탈출 가능 여부 판별" 2차 구현_반례 해결

--- Algorithm/dfsbfs/SaftyWay_Snake.py
+++ Algorithm/dfsbfs/SaftyWay_Snake.py
@@ -11,22 +11,24 @@ for i in range(n):
     showMap.append(getList)
 
 checkMap = [[False for _ in range(m)] for _ in range(n)]
-#checkMap[0][0] = True  
 
+# 결과를 담는 변수
 findWay = 0
 
 def dfs(a, b ,showMap, checkMap):
     global findWay
-    if -1<a<n and -1<b<n:
-        if not checkMap[a][b] and showMap[a][b] == "1":
+    # map  내부에 위치한 경우만 동작 시행
+    if -1<a<n and -1<b<m:
+        if not checkMap[a][b] and showMap[a][b] == "1":    
+            # 끝에 도달한 경우 결과 변경
             if a == n-1 and b == m -1:
                 findWay = 1
             else:
+                # 지나간 여부를 체크 -> 해당 문제에서는 돌아가는 경우는 없으므로 필요 없는 코드
                 checkMap[a][b] = True
-                dfs(a-1,b, showMap,checkMap)
+                # 아래와 오른쪽으로만 이동하므로 해당 경우만 처리
                 dfs(a+1,b,showMap,checkMap)
                 dfs(a,b+1,showMap,checkMap)
-                dfs(a,b-1,showMap,checkMap)
 
     return findWay
 
