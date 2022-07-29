f1b0d0a1a30c49d0643b732ebe8c3befef0dea0a
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-07-29 21:53:14 +0900

    Fix : "폭탄 해제 작업" 2차 구현_반례 해결

--- WinderPrac_2022/IM/Greedy/BoomOut.py
+++ WinderPrac_2022/IM/Greedy/BoomOut.py
@@ -5,22 +5,28 @@ import heapq
 caseSize = int(input())
 
 lBoom = []
+maxTime = -1
 
 for _ in range(caseSize):
     get = list(map(int, input().split()))
-    heapq.heappush(lBoom,(-get[0],-get[1]))
+    heapq.heappush(lBoom,(-get[1],-get[0]))
+    maxTime = max(maxTime,get[1])
 
-sTime = 0
-eTime = 0
+eTime = maxTime
+saveBoom = []
 lTime = [i for i in range(caseSize + 1)]
 result = 0
+nowTime = True
 
-while lBoom:
-    nScore,nTime = heapq.heappop(lBoom)
-    nScore,nTime = -nScore,-nTime
-    if result == 0:
-        result += nScore
-        eTime = nTime-1
-    else:
-        if eTime > nTime:
-            result += nScore
+while eTime != 0 :
+    while lBoom and -lBoom[0][0] >= eTime:
+        nTime,nScore = heapq.heappop(lBoom)
+        nTime,nScore = -nTime, -nScore
+        saveBoom.append(nScore)
+    if len(saveBoom) != 0:
+        big = max(saveBoom)
+        result += big
+        saveBoom.remove(big)
+    eTime -= 1
+
+print(result)
