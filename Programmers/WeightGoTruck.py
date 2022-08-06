d168fcb703fca717ed722ad1daf773c373e4db77
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2022-08-06 14:52:48 +0900

    Fix  : "다리를 지나는 트럭" 2차 구현

--- Programmers/WeightGoTruck.py
+++ Programmers/WeightGoTruck.py
@@ -1,30 +1,47 @@
 # 다리를 지나는 트럭
 
+from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+
     if bridge_length == 1:
         answer += (1+len(truck_weights))
     else:
         bfTime = 1
+        onBridge = deque([truck_weights[0]])
         nowTruck = truck_weights[0]
         for truck in truck_weights[1:]:
             if nowTruck + truck <= weight:
                 bfTime += 1
+                nowTruck += truck
+                onBridge.append(truck)
             else:
-                bfTime += bridge_length
-            nowTruck = truck
+                if len(onBridge) == 1:
+                    bfTime += bridge_length
+                    nowTruck = truck
+                else:
+                    while len(onBridge) != 1:
+                        nowTruck -= onBridge[0]
+                        onBridge.popleft()
+                        if nowTruck + truck <= weight:
+                            break
+                    if len(onBridge) == 1:
+                        bfTime += bridge_length
+                        onBridge.popleft()
+                    else:
+                        nowTruck += truck
+                        bfTime += 1
+                    onBridge.append(truck)
         bfTime += bridge_length
         answer = bfTime
-
-
-
     return answer
 
 
-#l = 2
-l = 100
-#w = 10
-w = 100
-#t = [7,4,5,6]
-t = [10,10,10,10,10,10,10,10,10,10]
+l = 2
+#l = 100
+w = 10
+#w = 100
+t = [7,4,5,6]
+#t = [10,10,10,10,10,10,10,10,10,10]
 print(solution(l,w,t))
