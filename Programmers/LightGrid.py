f27989202cc25a6cfd00db755c84b000bbab8230
lungnahahd <jyoungjk33@gmail.com>
2022-08-11 08:01:41 +0900

    Add : "빛의 경로 사이클"

---
+++ Programmers/LightGrid.py
@@ -0,0 +1,20 @@
+# 빛의 경로 사이클
+
+def solution(grid):
+    answer = []
+    nodeWay = [] # 모든 노드에서의 동서남북을 기록할 변수 
+    for node in grid:
+
+
+
+    return answer
+
+
+
+
+
+
+
+grid = ["SL","LR"]
+
+print(solution(grid))
