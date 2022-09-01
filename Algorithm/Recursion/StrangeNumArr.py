082262e697985bec652a10b9f06128c3258cc278
lungnahahd <jyoungjk33@gmail.com>
2022-09-02 06:44:51 +0900

    Fix : "이상한 수열" 재귀 함수 코드 변경

--- Algorithm/Recursion/StrangeNumArr.py
+++ Algorithm/Recursion/StrangeNumArr.py
@@ -1,18 +1,17 @@
 # 이상한 수열(코드 트리)
 
+from pickletools import read_unicodestring1
+
+
 n = int(input())
 
-num_array = [0,1,2]
-
-def makeArr(na):
-    global n
-    if n == 1 or n == 2:
-        return na[n]
-    one = na[(len(na) // 3)]
-    two = na[len(na)-1]
-    na.append(one+two)
-    if len(na) == n+1:
-        return na[-1]
+def makeArr(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
     else:
-        return makeArr(na)
-print(makeArr(num_array))
+        return makeArr(n//3) + makeArr(n-1)
+
+
+print(makeArr(n))
