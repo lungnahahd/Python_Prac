989c48111469fcc0b552874de669f72edf691583
lungnahahd <jyoungjk33@gmail.com>
2022-09-17 00:39:03 +0900

    Fix : "방금그곡" 2차구현

--- Programmers/Recent_Song.py
+++ Programmers/Recent_Song.py
@@ -16,9 +16,13 @@ def solution(m, musicinfos):
     for i in musicinfos:
         music = i.split(",")
         music[3] = change_str(music[3])
-        end = int(''.join(music[1].split(":")))
-        start = int(''.join(music[0].split(":")))
-        song_time = end - start
+        end = ''.join(music[1].split(":"))
+        start = ''.join(music[0].split(":"))
+
+        if end[:2] != start[:2]:
+            song_time = ((60 - int(start[2:])) + int(end[2:]) + 60*(int(end[:2]) - int(start[:2]) - 1))
+        else:
+            song_time = int(end) - int(start)
         check = ""
         for time in range(song_time):
             check += music[3][time%len(music[3])]
@@ -38,5 +42,5 @@ def solution(m, musicinfos):
 
 print(solution("ABCDEFG",["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
 print(solution("CC#BCC#BCC#BCC#B",["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
-print(solution("ABC",	["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
-print(solution("qweqweqwe",	["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("ABC",["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("ABCDEFG", ["11:50,12:04,HELLO,CDEFGAB", "12:57,13:11,BYE,CDEFGAB"]))
