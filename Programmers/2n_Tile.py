d272064a0f254b515836911fa6976899ebaf6983
lungnahahd <jyoungjk33@gmail.com>
2022-09-17 19:05:14 +0900

    Fix : "2xn 타일링" 2차 구현_반례 해결

--- Programmers/2n_Tile.py
+++ Programmers/2n_Tile.py
@@ -6,7 +6,7 @@ def solution(n):
     temp_save = [0,1,2]
     if n > 2:
         for i in range(3,n+1):
-            sum_num = temp_save[i-2] + temp_save[i-1] % 1000000007
+            sum_num = (temp_save[i-2] + temp_save[i-1]) % 1000000007
             temp_save.append(sum_num)
     return temp_save[n]
 
