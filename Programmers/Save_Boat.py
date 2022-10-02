ae6c17283742c86675518e5e9b2e9c5e69444080
lungnahahd <jyoungjk33@gmail.com>
2022-10-02 21:46:44 +0900

    Add : "구명보트" 1차 구현_반례 존재

---
+++ Programmers/Save_Boat.py
@@ -0,0 +1,33 @@
+# 구명 보트
+
+def solution(people, limit):
+    answer = 0
+    people = sorted(people)
+    big,small = len(people)-1,0
+    weight,save_num = 0,0
+    while big > small:
+        if weight == 0:
+            weight += people[big]
+            big -= 1
+            save_num += 1
+
+        if weight < limit:
+            #weight = min(limit,weight+people[small])
+            #print(f'여기의 무게: {weight}')
+            temp = min(limit,weight+people[small])
+            if weight+people[small] == limit or temp != limit:
+                small += 1
+                save_num += 1
+            weight = temp
+        if weight == limit:
+            weight = 0
+            answer += 1
+
+    if save_num != len(people):
+        #print(small,big)
+        answer += 1
+
+    return answer
+
+print(solution([70, 50, 80, 50],10000))
+print(solution([70, 80, 50],100))
