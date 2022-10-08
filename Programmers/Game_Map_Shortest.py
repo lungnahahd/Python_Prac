4facb13f30dd7aa81007867c914c234398fbcad9
lungnahahd <jyoungjk33@gmail.com>
2022-10-09 01:27:13 +0900

    Fix : "게임 맵 최단거리" 2차 구현

--- Programmers/Game_Map_Shortest.py
+++ Programmers/Game_Map_Shortest.py
@@ -2,27 +2,31 @@
 
 # (행, 열)
 
-row_move = [-1,+1,0,0]
-col_move = [0,0,+1,-1]
+row_move = [-1,0,+1,0]
+col_move = [0,+1,0,-1]
 go_dist = []
+min_way = 101
 
 def solution(maps):
     global go_dist
+    global min_way
     go_dist = []
     max_row, max_col = len(maps)-1,len(maps[0])-1 # 적팀 진영의 위치
     visited = set() # 방문 여부를 저장할 딕셔너리
     find_way(maps,0,0,1,visited)
 
     if len(go_dist) == 0:
+        min_way = 101
         return -1
     else:
+        min_way = 101
         return(min(go_dist))
-    print(go_dist)
-
-    return answer
 
 def find_way(map,row,col,distance,visited):
     global go_dist
+    global min_way
+    if distance >= min_way:
+        return
     for cnt in range(4):
         row_temp = row_move[cnt] + row
         col_temp = col_move[cnt] + col
@@ -33,8 +37,10 @@ def find_way(map,row,col,distance,visited):
                 now_dist += 1
                 now_visit.add((row_temp,col_temp))
                 if row_temp == (len(map)-1) and col_temp == (len(map[0])-1):
+                    min_way = now_dist
                     go_dist.append(now_dist)
-                find_way(map,row_temp,col_temp,now_dist,now_visit)
+                else:
+                    find_way(map,row_temp,col_temp,now_dist,now_visit)
 
 
 
