628f4ecd7f3d60ee1a6d5aab9577367d0d45643a
lungnahahd <jyoungjk33@gmail.com>
2022-10-10 02:41:21 +0900

    Fix : "H-Index" 3차 구현_반례 해결

--- Programmers/H_Index.py
+++ Programmers/H_Index.py
@@ -3,16 +3,12 @@
 def solution(citations):
     answer = 0
     citations.sort(reverse=True)
-    save_many = dict()
-    for idx,cite in enumerate(citations):
-        save_many[cite] = idx+1
     cite_len = len(citations)
-    print(save_many)
     before_paper = citations[0]
     for idx in range(cite_len):
         paper = citations[idx]
         blank = before_paper - paper
-        if blank > 0:
+        if blank > 0: # 중간 논문 개수를 처리하기 위한 조건 -> 반복문으로 중간이 없으면 지나감
             for minus in range(1,blank):
                 temp = before_paper - minus
                 if temp <= idx:
@@ -20,4 +16,12 @@ def solution(citations):
         if paper <= idx+1:
             answer = max(paper,answer)
         before_paper = paper
+    blank = before_paper 
+    # 마지막까지 나오지 않는 경우 처리
+    ## [3,3] 같은 경우
+    if blank > 0:
+        for minus in range(1,blank):
+            temp = before_paper - minus
+            if temp <= cite_len:
+                answer = max(temp,answer)
     return answer
