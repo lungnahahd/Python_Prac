f0d18fe76c6c9f0016d433f3afc5c518b143628f
lungnahahd <jyoungjk33@gmail.com>
2022-10-10 02:33:59 +0900

    Fix : "H-Index" 2차 구현

--- Programmers/H_Index.py
+++ Programmers/H_Index.py
@@ -3,8 +3,21 @@
 def solution(citations):
     answer = 0
     citations.sort(reverse=True)
+    save_many = dict()
+    for idx,cite in enumerate(citations):
+        save_many[cite] = idx+1
     cite_len = len(citations)
+    print(save_many)
+    before_paper = citations[0]
     for idx in range(cite_len):
         paper = citations[idx]
-        if paper-1 <= idx and (cite_len-idx-1) <= paper:
-            return paper
+        blank = before_paper - paper
+        if blank > 0:
+            for minus in range(1,blank):
+                temp = before_paper - minus
+                if temp <= idx:
+                    answer = max(temp,answer)
+        if paper <= idx+1:
+            answer = max(paper,answer)
+        before_paper = paper
+    return answer
