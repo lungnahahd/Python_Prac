2ae1aa3c4132216dadac2512d60879e9c643d35e
lungnahahd <jyoungjk33@gmail.com>
2022-10-12 03:02:38 +0900

    Fix : "이진 변환 반복하기" 2차 구현_반례 해결

--- Programmers/Bin_Repeat.py
+++ Programmers/Bin_Repeat.py
@@ -4,10 +4,11 @@ def get_rid_zero(s):
     result = ""
     count = 0
     for i in s:
-        if s == '1':
-            result += "1"
+        if i == '1':
+            result = result + "1"
         else:
             count += 1
+    print(result)
     return count, result
 
 def solution(s):
@@ -24,8 +25,9 @@ def solution(s):
         return answer
     s_temp = len(s_temp)
     s_temp = bin(s_temp)[2:]
+    print(s_temp)
     while s_temp != '1':
-        if s_temp.count('0') != 1:
+        if s_temp.count('0') != 0:
             temp_count,s_temp = get_rid_zero(s_temp)
             count += temp_count
         s_temp = bin(len(s_temp))[2:]
