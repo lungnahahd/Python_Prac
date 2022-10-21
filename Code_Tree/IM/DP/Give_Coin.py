08e8b255fe49f7583ec94f24f2a558567afecff4
lungnahahd <jyoungjk33@gmail.com>
2022-10-21 23:59:39 +0900

    Add : "동전 거슬러주기" 1차 구현

---
+++ Code_Tree/IM/DP/Give_Coin.py
@@ -0,0 +1,22 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+coin, money = map(int,input().split())
+
+kind = list(map(int,input().split()))
+
+num = [INT_MAX for _ in range(10001)]
+
+for i in kind:
+    num[i] = 1
+
+for idx in range(1,money+1):
+    for basic in kind:
+        if idx - basic > 0:
+            num[idx] = min(num[idx],num[basic] + num[idx-basic])
+
+if num[money] == INT_MAX:
+    print(-1)
+else:
+    print(num[money])
