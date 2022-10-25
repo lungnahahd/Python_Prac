72e619648ad25572d92a515159aca6139a377a5f
lungnahahd <jyoungjk33@gmail.com>
2022-10-26 00:38:12 +0900

    Fix : "계단 오르기" 2차 구현_반례 발생

--- backjoon/DP/Stair_Up.py
+++ backjoon/DP/Stair_Up.py
@@ -9,8 +9,19 @@ for _ in range(size):
 
 dp = [[0,0,0] for _ in range(size-1)]
 dp[0][1] = stairs[0]
+two_step = False
+if size == 1:
+    
 for step in range(1,size-1):
-    dp[step][0] = max(dp[step-1])
+    if two_step:
+        dp[step][0] = max(dp[step-1][1:])
+        two_step = False
+    else:
+        dp[step][0] = max(dp[step-1])
+        if dp[step][0] == dp[step-1][2] and dp.count(dp[step-1]) == 1:
+            two_step = True
+        else:
+            two_step = False
     dp[step][1] = dp[step-1][0] + stairs[step]
     dp[step][2] = dp[step-1][1] + stairs[step]
 
@@ -18,3 +29,17 @@ for step in range(1,size-1):
 
 answer = stairs[-1] + max(dp[size-2][0],dp[size-2][1])
 print(answer)
+
+## 반례
+# 10
+# 3
+# 5
+# 10 
+# 9
+# 2
+# 1
+# 2
+# 5
+# 2
+# 9
+# 38
