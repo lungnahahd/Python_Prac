1a8bdc34e5adf6040b19eb0f967249e845b804bd
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2023-11-23 23:34:32 +0900

    Fix : "최대로 연속한 숫자"
    시간 초과 발생

--- Code_Tree/IM/TreeSet/Max_Range.py
+++ Code_Tree/IM/TreeSet/Max_Range.py
@@ -1,38 +1,28 @@
 max_num, remove_cnt = input().split()
 max_num, remove_cnt =  int(max_num), int(remove_cnt)
 remove_nums = list(map(int,input().split()))
-num_list = [[idx for idx in range(max_num+1)]]
+num_list = [(0,max_num)]
 big_range = 0
 
 for idx in range(remove_cnt):
     now_remove = remove_nums[idx]
     size = len(num_list)
     for now in range(size):
-        now_list = num_list.pop(0)
-        start, end = now_list[0], now_list[-1]
+        start,end = num_list.pop(0)
         if (start > now_remove or end < now_remove):
-            num_list.append(now_list)
+            num_list.append((start,end))
             big_range = max(big_range, end-start+1)
         elif (start == now_remove):
-            now_list.remove(start)
             big_range = max(big_range, end-start)
-            if(len(now_list) != 0):
-                num_list.append(now_list)
+            if(start != end ):
+                num_list.append((start+1, end))
         elif (end == now_remove):
-            now_list.remove(end)
             big_range = max(big_range, end-start)
-            if(len(now_list) != 0):
-                num_list.append(now_list)
+            if(start != end):
+                num_list.append((start, end-1))
         else:
-            s_temp, e_temp = [], []
-            for temp_num in now_list:
-                if temp_num < now_remove:
-                    s_temp.append(temp_num)
-                elif temp_num > now_remove:
-                    e_temp.append(temp_num)
-            
-            num_list.append(s_temp)
-            num_list.append(e_temp)
-            big_range = max(big_range, len(s_temp), len(e_temp))
+            num_list.append((start, now_remove-1))
+            num_list.append((now_remove+1, end))            
+            big_range = max(big_range, now_remove-start, end-now_remove)
     print(big_range)
     big_range = 0
