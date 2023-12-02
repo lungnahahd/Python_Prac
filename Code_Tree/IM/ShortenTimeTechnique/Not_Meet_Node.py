81608071b5420fff84486b0a5598fa7419942c9c
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2023-12-02 16:27:38 +0900

    Fix: "만나지 않는 선분"
    예외 처리 못함

--- Code_Tree/IM/ShortenTimeTechnique/Not_Meet_Node.py
+++ Code_Tree/IM/ShortenTimeTechnique/Not_Meet_Node.py
@@ -1,39 +1,37 @@
 import heapq
 
 node_cnt = int(input())
-node_check = [True for _ in range(node_cnt)]
-node_in_line = set()
-node_out = []
-line_list = []
-top = -1
-temp, answer = 0,0
+temp, sort_line = [], []
+in_node = set()
+not_finish_line = []
+finish = [True for _ in range(node_cnt)]
 
-for idx in range(node_cnt):
-    x, y = tuple(map(int, input().split()))
-    heapq.heappush(line_list, (x,-1,idx))
-    heapq.heappush(line_list, (y,+1,idx))
-    heapq.heappush(node_out, idx)
+for _ in range(node_cnt):
+    x,y = tuple(map(int, input().split()))
+    heapq.heappush(temp, (x,y))
 
-while (line_list):
-    node, val, line = heapq.heappop(line_list)
-    temp += val
-    if(val == +1):
-        node_in_line.remove(line)
-        if (temp <= 0 and node_out[0] == line):
-            answer += 1
-            print(node)
-            print(line)
-            
-            heapq.heappop(node_out)
-            print(node_out)
-            print("------")
+sort_cnt = 0
+while temp:
+    x,y = heapq.heappop(temp)
+    heapq.heappush(sort_line, (x,sort_cnt))
+    heapq.heappush(sort_line, (y,sort_cnt))
+    heapq.heappush(not_finish_line, sort_cnt)
+    sort_cnt += 1
 
-        else:
-            node_check[line] = False
-            node_out.remove(line)
-            for in_node in node_in_line:
-                node_check[in_node] = False
-    else:
-        node_in_line.add(line)
-
+answer = 0
+while sort_line:
+    node, line = heapq.heappop(sort_line)
+    if line not in in_node:
+        in_node.add(line)
+    elif line in in_node:
+        if (finish[line]):
+            if (not_finish_line[0] == line):
+                heapq.heappop(not_finish_line)
+                answer += 1
+            else:
+                while True:
+                    now_node = heapq.heappop(not_finish_line)
+                    finish[now_node] = False
+                    if (now_node == line):
+                        break
 print(answer)
