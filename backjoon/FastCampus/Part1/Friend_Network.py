cbee73946033660ae67d2d68219a7d9c1dfa15cd
lungnahahd <jyoungjk33@gmail.com>
2023-12-24 17:07:44 +0900

    Fix : "친구 네트워크" 난이도 : 중 시간 초과 발생

--- backjoon/FastCampus/Part1/Friend_Network.py
+++ backjoon/FastCampus/Part1/Friend_Network.py
@@ -1,26 +1,52 @@
 # 친구 네트워크 (4195)
 
-case_num = int(input())
+case = int(input())
 
-answer = []
-for _ in range(case_num):
-    cnt = int(input())
-    friend_dict = dict()
-    for _ in range(cnt):
-        temp_answer = 0
+# 최종 부모를 세팅하는 함수
+def change_mother(set_child, group):
+    for child in set_child:
+        final_paranet = ''
+        temp_group = []
+        while True:
+            if (group[child] == child):
+                break
+            else:
+                temp_group.append(child)
+                final_paranet = group[child]
+                child = group[child]
+        while temp_group:
+            now = temp_group.pop()
+            group[now] = final_paranet
+
+# 각 노드의 부모를 찾는 과정 
+def find_mother(child, group):
+    if child not in group:
+        group[child] = child
+        return child
+    else:
+        if group[child] == child:
+            return child
+        else:
+            return find_mother(group[child], group)
+
+
+for _ in range(case):
+    cnt_net = int(input())
+    group = dict()
+    set_child = set()
+    answer = []
+    for _ in range(cnt_net):
         left, right = input().split()
-        
-        
-        temp_left, temp_right = set([left]), set([right])
-
-        if left in friend_dict:
-            temp_left = friend_dict[left]
-        if right in friend_dict:
-            temp_right = friend_dict[right]        
-        
-        temp_union = temp_left.union(temp_right)
-        answer.append(len(temp_union))
-        friend_dict[left], friend_dict[right] = temp_union, temp_union
-
-for relation in answer:
-    print(relation)
+        left_parent = find_mother(left, group)
+        right_parent = find_mother(right, group)
+        set_child.add(left)
+        set_child.add(right)
+        group[right] = left_parent  
+        change_mother(set_child, group)
+        now_cnt = 0
+        for key in group:
+            if group[key] == find_mother(left, group):
+                now_cnt += 1
+        answer.append(now_cnt)
+    for rst in answer:
+        print(rst)
