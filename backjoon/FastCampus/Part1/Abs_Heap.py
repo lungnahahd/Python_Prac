8bcf87051f6f3ca95eb7e93049011db3d345394f
lungnahahd <jyoungjk33@gmail.com>
2024-01-04 22:27:38 +0900

    Fix : "절대값 힙" - 난이도 : 중 - 빠른 입력 함수로 문제 해결

--- backjoon/FastCampus/Part1/Abs_Heap.py
+++ backjoon/FastCampus/Part1/Abs_Heap.py
@@ -1,27 +1,28 @@
 # 절댓값 힙 (11286)
 ## 난이도 : 중
 
+import sys
 import heapq
-from collections import deque
 
+input = sys.stdin.readline
 cnt_cmd = int(input())
 
-arr = deque([])
+arr = []
 result = []
-
 for _ in range(cnt_cmd):
     cmd = int(input())
     if (cmd == 0):
         if len(arr) == 0:
             result.append(0)
         else:
-            abs_val, val = arr.popleft()
-            #abs_val, val = heapq.heappop(arr)
+            abs_val, val = heapq.heappop(arr)
             result.append(val)
     else:
-        arr.append((abs(cmd), cmd))
-        arr = deque(sorted(arr))
-        #heapq.heappush(arr, (abs(cmd),cmd))
+        heapq.heappush(arr, (abs(cmd),cmd))
 
 for num in result:
     print(num)
+
+
+
+from collections import deque
