3b83cecc336097b3b515b868bf3dd9ee1ee4cf5f
lungnahahd <jyoungjk33@gmail.com>
2024-01-04 22:25:19 +0900

    Fix : "절댓값 힙" - 난이도 : 중 - deque 활용했지만 시간초과 발생

--- backjoon/FastCampus/Part1/Abs_Heap.py
+++ backjoon/FastCampus/Part1/Abs_Heap.py
@@ -2,21 +2,26 @@
 ## 난이도 : 중
 
 import heapq
+from collections import deque
 
 cnt_cmd = int(input())
 
-arr = []
+arr = deque([])
 result = []
+
 for _ in range(cnt_cmd):
     cmd = int(input())
     if (cmd == 0):
         if len(arr) == 0:
             result.append(0)
         else:
-            abs_val, val = heapq.heappop(arr)
+            abs_val, val = arr.popleft()
+            #abs_val, val = heapq.heappop(arr)
             result.append(val)
     else:
-        heapq.heappush(arr, (abs(cmd),cmd))
+        arr.append((abs(cmd), cmd))
+        arr = deque(sorted(arr))
+        #heapq.heappush(arr, (abs(cmd),cmd))
 
 for num in result:
     print(num)
