ee42801a33682e52f09b5e4d93efcf9adec390a1
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2024-01-24 21:49:33 +0900

    Fix : "문서 검색"
    시간 초과 발생

--- backjoon/FastCampus/Part2/FindDoct.py
+++ backjoon/FastCampus/Part2/FindDoct.py
@@ -10,15 +10,19 @@ find = list(input())
 
 now_idx = 0
 result = 0
+back_idx, go_idx = 1, 0
 
-for word in doct:
-    if word == doct[now_idx]:
+while go_idx < len(doct):
+    if doct[go_idx] == find[now_idx]:
         if now_idx == len(find):
             result += 1
+            back_idx = go_idx + 2
+            now_idx = 0
         else:
             now_idx += 1
     else:
         now_idx = 0
+        go_idx = back_idx
 
 print(result)
 
