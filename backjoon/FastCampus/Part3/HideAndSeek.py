f5e97d0a6c59431e8280761f06b12640f1d51a67
Lungnaha <67555400+lungnahahd@users.noreply.github.com>
2024-02-23 00:39:28 +0900

    Fix : 숨박꼭질
    bfs로 문제 해결

--- backjoon/FastCampus/Part3/HideAndSeek.py
+++ backjoon/FastCampus/Part3/HideAndSeek.py
@@ -1,6 +1,7 @@
 # 숨박꼭질 (1697)
 
 import sys
+from collections import deque
 
 MAX_INT = sys.maxsize
 input = sys.stdin.readline
@@ -8,32 +9,38 @@ input = sys.stdin.readline
 start, end = list(map(int, input().split()))
 visited = set()
 visited.add(start)
-result = MAX_INT
+save = deque([(start,0)])
+result = 0
 
-def bfs(node):
+def bfs():
     global visited
     global result
     global start
     global end
+    global save
     
-    if (node-1 > -1 and node-1 not in visited):
-        if (node-1 == end):
-            result = min(result, node-1)
-            return
-        visited.add(node-1)
-        bfs(node-1)
-    if (node+1 <= 100000 and node+1 not in visited):
-        if (node+1 == end):
-            result = min(result, node+1)
-            return
-        visited.add(node+1)
-        bfs(node+1)
-    if (node*2 <= 100000 and node*2 not in visited):
-        if (node*2 == end):
-            result = min(result, node*2)
-            return
-        visited.add(node*2)
-        bfs(node*2)
+    while save:
+        node, cnt = save.popleft()
+        cnt += 1
+        
+        if (node-1 > -1 and node-1 not in visited):
+            if (node-1 == end):
+                result = cnt
+                break
+            visited.add(node-1)
+            save.append((node-1, cnt))
+        if (node+1 <= 100000 and node+1 not in visited):
+            if (node+1 == end):
+                result = cnt
+                break
+            visited.add(node+1)
+            save.append((node+1, cnt))
+        if (node*2 <= 100000 and node*2 not in visited):
+            if (node*2 == end):
+                result = cnt
+                break
+            visited.add(node*2)
+            save.append((node*2,cnt))
 
-bfs(start)
+bfs()
 print(result)
