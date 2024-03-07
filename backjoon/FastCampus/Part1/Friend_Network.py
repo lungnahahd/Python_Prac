9364512cbe863828315c5e7e3bea7d1bcfed3289
lungnahahd <jyoungjk33@gmail.com>
2024-03-08 00:36:43 +0900

    Fix : 친구네트워크 풀이 진행 중..

--- backjoon/FastCampus/Part1/Friend_Network.py
+++ backjoon/FastCampus/Part1/Friend_Network.py
@@ -1,52 +1,36 @@
 # 친구 네트워크 (4195)
 
+import sys
+
+input = sys.stdin.readline
+
 case = int(input())
 
-# 최종 부모를 세팅하는 함수
-def change_mother(set_child, group):
-    for child in set_child:
-        final_paranet = ''
-        temp_group = []
-        while True:
-            if (group[child] == child):
-                break
-            else:
-                temp_group.append(child)
-                final_paranet = group[child]
-                child = group[child]
-        while temp_group:
-            now = temp_group.pop()
-            group[now] = final_paranet
+def union(a, b):
+    a_mom = find(a)
+    b_mom = find(b)
 
-# 각 노드의 부모를 찾는 과정 
-def find_mother(child, group):
-    if child not in group:
-        group[child] = child
-        return child
+    if(a < b):
+        mother[b] = a_mom
     else:
-        if group[child] == child:
-            return child
-        else:
-            return find_mother(group[child], group)
+        mother[a] = b_mom
+
+def find(node):
+    if mother[node] != node:
+        return find(mother[node])
+    else:
+        return mother[node]
 
 
 for _ in range(case):
-    cnt_net = int(input())
-    group = dict()
-    set_child = set()
-    answer = []
-    for _ in range(cnt_net):
-        left, right = input().split()
-        left_parent = find_mother(left, group)
-        right_parent = find_mother(right, group)
-        set_child.add(left)
-        set_child.add(right)
-        group[right] = left_parent  
-        change_mother(set_child, group)
-        now_cnt = 0
-        for key in group:
-            if group[key] == find_mother(left, group):
-                now_cnt += 1
-        answer.append(now_cnt)
-    for rst in answer:
-        print(rst)
+    cnt_network  = int(input())
+    mother = dict()
+    cnt = 0
+    for _ in range(cnt_network):
+        a, b = input().split()
+        if 1 not in mother:
+            mother[a] = a
+        if b not in mother:
+            mother[b] = b
+        union(a,b)
+    print(mother)
