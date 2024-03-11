800687552fce949809065a3ab7ea4293ad1f57b8
khd <jyoungjk33@gmail.com>
2024-03-12 01:41:34 +0900

    Fix : 거의 최단 경로 1차 구현 틀림

--- backjoon/FastCampus/Part3/AlmostNearPath.py
+++ backjoon/FastCampus/Part3/AlmostNearPath.py
@@ -8,30 +8,47 @@ from collections import deque
 
 input = sys.stdin.readline
 INT_MAX = sys.maxsize
+SHORT_DIS = sys.maxsize
+finish = False
 
-def find_short(start, cnt):
+def find_short(start, end, cnt, flag):
+    global short_dis
+    global finish
+
     val_table = [[INT_MAX,[]] for idx in range(cnt)]
 
-    save = deque([(start, 0)])
+    save = deque([(start, 0, [start])])
     val_table[start][0] = 0
     val_table[start][1] = [start]
-    #val_table[start] = (0, [start])
 
     while save:
-        now_node, now_cost = save.popleft()
+        now_node, now_cost, path = save.popleft()
 
         for next_node, next_cost in cost_map[now_node]:
             if val_table[next_node][0] > now_cost + next_cost:
-                temp = copy.deepcopy(val_table[next_node][1])
+                temp = copy.deepcopy(path)
                 temp.append(next_node)
                 val_table[next_node][0], val_table[next_node][1] = now_cost+next_cost, temp
                 
-                save.append((next_node, now_cost + next_cost))
+                save.append((next_node, now_cost + next_cost, temp))
+
+    if flag:
+        if short_dis == val_table[end][0]:
+            finish = True    
+        short_dis = val_table[end][0]
+        return get_rid_of(val_table[end][1])
+    else:
+        return val_table
+
+
+def get_rid_of(target):
+    for idx in range(1, len(target)):
+        for tg, tg_val in cost_map[target[idx-1]]:
+            if tg == target[idx]:
+                cost_map[target[idx-1]].remove((tg, tg_val))
+                break
     
-    return val_table
-
-
-
+    return cost_map
 
 while True:
     place_cnt, road_cnt = list(map(int, input().split())) # 각 케이스의 시작
@@ -43,6 +60,17 @@ while True:
         a_node, b_node, value = list(map(int, input().split()))
         cost_map[a_node].append((b_node, value))
     
-    test = find_short(start_node, place_cnt)
-    print(test)
+    short_dis = sys.maxsize
+    
+    cost_map = find_short(start_node, end_node, place_cnt, True)
 
+    while not finish:
+        cost_map = find_short(start_node, end_node, place_cnt, True)
+        if short_dis == sys.maxsize:
+            break
+
+    answer = find_short(start_node, end_node, place_cnt, False)
+    if answer[end_node][0] == sys.maxsize:
+        print(-1)
+    else:
+        print(answer[end_node][0])
