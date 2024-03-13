7636f21b3e138ad471d13efe88217db0bbf87485
khd <jyoungjk33@gmail.com>
2024-03-14 00:40:40 +0900

    Add : 배 틀림

---
+++ backjoon/FastCampus/Part3/Boat.py
@@ -0,0 +1,30 @@
+# 배 (1092)
+## 난이도 : 중
+
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+crain_cnt = int(input())
+crains = list(map(int, input().split()))
+crains.sort(reverse=True)
+box_cnt = int(input())
+temp = list(map(int, input().split()))
+boxs = []
+for box in temp:
+    heapq.heappush(boxs, -box)
+
+rst_time = 0
+
+while boxs:
+    for crain in crains:
+        if not boxs:
+            break
+        if crain >= -boxs[0]:
+            heapq.heappop(boxs)
+        else:
+            break
+
+    rst_time += 1
+print(rst_time)
