9826baa7131be7c08698ec8ff869d0cfe130416a
khd <jyoungjk33@gmail.com>
2024-03-14 01:29:38 +0900

    Fix : 배 시간 초과 발생

--- backjoon/FastCampus/Part3/Boat.py
+++ backjoon/FastCampus/Part3/Boat.py
@@ -3,6 +3,7 @@
 
 import sys
 import heapq
+from collections import deque
 
 input = sys.stdin.readline
 
@@ -10,29 +11,29 @@ crain_cnt = int(input())
 crains = list(map(int, input().split()))
 crains.sort(reverse=True)
 box_cnt = int(input())
-temp = list(map(int, input().split()))
-boxs = []
-for box in temp:
-    heapq.heappush(boxs, -box)
+boxs  = list(map(int, input().split()))
+boxs.sort(reverse=True)
+#boxs = []
+
+# for box in temp:
+#     heapq.heappush(boxs, -box)
+
+
 
 rst_time = 0
 
-while boxs:
-    for crain in crains:
-        temp = []
-        while boxs:
-            now_box = heapq.heappop(boxs)
-            if -now_box <= crain:
-                break
-            temp.append(now_box)
-        boxs = temp + boxs
-        heapq.heapify(boxs)
-        
+if boxs[0] > crains[0]:
+    rst_time = -1
+else:
+    dq_boxs = deque(boxs)
 
-    if boxs and -boxs[0] > crains[0]:
-        rst_time = -1
-        break
+    while dq_boxs:
+        for crain in crains: 
+            for box in dq_boxs:
+                if crain >= box:
+                    dq_boxs.popleft()
+                    break
+        rst_time += 1
 
-    rst_time += 1
     
 print(rst_time)
