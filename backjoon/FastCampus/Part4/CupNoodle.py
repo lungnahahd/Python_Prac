bf192c7234157b7b54e45778560f2e7e60f2806f
lungnahahd <jyoungjk33@gmail.com>
2024-03-21 00:14:43 +0900

    Fix : 컵라면 시간 초과 발생

--- backjoon/FastCampus/Part4/CupNoodle.py
+++ backjoon/FastCampus/Part4/CupNoodle.py
@@ -16,10 +16,26 @@ for _ in range(cnt):
 
 now_time = INT_MAX
 result = 0
+buffer = []
 
-while problems:
+while now_time != 1:
     temp_deadline, temp_cup_noodle = heapq.heappop(problems)
-    if -temp_deadline < now_time:
+    if -temp_deadline < now_time :
         now_time = -temp_deadline
         result += (-temp_cup_noodle)
+    else:
+        #if now_time != 1:
+        heapq.heappush(problems, (-(now_time-1),temp_cup_noodle))
+
+
+
 print(result)
+
+
+# 4
+# 1 50
+# 2 30
+# 3 60
+# 3 70
+
+# 180
