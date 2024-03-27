c00053bd0f89eb3ab31cf643c2404a891a466d5b
khd <jyoungjk33@gmail.com>
2024-03-27 23:33:56 +0900

    Fix : 선 긋기 정렬 활용 문제 해결

--- backjoon/FastCampus/Part4/LineMake.py
+++ backjoon/FastCampus/Part4/LineMake.py
@@ -13,11 +13,13 @@ state, start = 0, 0
 
 for _ in range(cnt_line):
     x, y = list(map(int, input().split()))
-    heapq.heappush(num_save, (x, -1))
-    heapq.heappush(num_save, (y, +1))
+    num_save.append((x,-1))
+    num_save.append((y,+1))
+
+num_save.sort(reverse=True)
 
 while num_save:
-    now_node, now_val = heapq.heappop(num_save)
+    now_node, now_val = num_save.pop()
     if now_val == -1:
         if state == 0:
             start = now_node
@@ -25,6 +27,6 @@ while num_save:
     else:
         state += now_val
         if state == 0:
-            result += abs(now_val - start)
+            result += abs(now_node - start)
 print(result)
         
