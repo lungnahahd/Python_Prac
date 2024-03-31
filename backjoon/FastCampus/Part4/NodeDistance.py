12c91d175c13c62bd11a1885bb09d5a4217501d5
lungnahahd <jyoungjk33@gmail.com>
2024-03-31 12:21:38 +0900

    Add : 노드 사이의 거리 bfs로 시간초과 해결

--- backjoon/FastCampus/Part4/NodeDistance.py
+++ backjoon/FastCampus/Part4/NodeDistance.py
@@ -9,33 +9,33 @@ input = sys.stdin.readline
 cnt_node, cnt_line = list(map(int, input().split()))
 
 result = []
-graph = [[0 for _ in range(cnt_node + 1)] for _ in range(cnt_node + 1)]
+graph = [[] for _ in range(cnt_node + 1)]
 
 for _ in range(cnt_node-1):
     x, y, cost = list(map(int, input().split()))
-    graph[x][y] = cost
-    graph[y][x] = cost
+    graph[x].append((y,cost))
+    graph[y].append((x,cost))
 
 def bfs(x,y):
     global graph, result
     visited = set()
     stack = deque([])
     stack.append((x, 0))
+    visited.add(x)
 
     while stack:
         now_start, mid_rst = stack.popleft()
-        for idx in range(1, cnt_node+1):
-            if graph[now_start][idx] != 0:
-                if idx == y:
-                    return mid_rst + graph[now_start][idx]
+        for next_node, cost in graph[now_start]:
+            if next_node not in visited:
+                if next_node == y:
+                    return mid_rst + cost
                 else:
-                    if idx not in visited:
-                        stack.append((idx, mid_rst + graph[now_start][idx]))
-        visited.add(now_start)
+                    visited.add(next_node)
+                    stack.append((next_node, mid_rst + cost))
 
 for _ in range(cnt_line):
     node_one, node_two = list(map(int, input().split()))
-    result.append(bfs(min(node_one, node_two), max(node_one, node_two)))
+    result.append(bfs(node_two, node_one))
 
 for val in result:
     print(val)
