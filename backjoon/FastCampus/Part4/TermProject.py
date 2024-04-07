e5c3ca018ee8b1100e43782c6cef13a976d22739
lungnahahd <jyoungjk33@gmail.com>
2024-04-07 21:14:04 +0900

    Fix : 텀 프로젝트 틀림...

--- backjoon/FastCampus/Part4/TermProject.py
+++ backjoon/FastCampus/Part4/TermProject.py
@@ -6,43 +6,47 @@ import sys
 case_cnt = int(input())
 result = []
 cycle_member = set()
+isCycle = False
 
 
-
-def dfs(start, graph):
-    global cycle_member, visited
-    stack = []
-    stack.append((start,start))
-    isCycle = False
-    cycle_mother = -1
-    while stack:
-        node, mother = stack.pop()
-        next_node = graph[node] - 1
-        if not visited[next_node]:
-            stack.append((next_node, node))
-            visited[next_node] = True
-        else:
-            if not isCycle:
-                if next_node == node:
-                    cycle_member.add(next_node)
-                else:
-                    isCycle = True
-                    cycle_member.add(next_node)
-                    cycle_mother = next_node
-            else:
-                if cycle_mother == next_node:
-                    isCycle = False    
+def dfs(start, graph, cycle_mother):
+    global cycle_member, visited, isCycle, finished
+    
+    visited[start] = True
+    next_node = graph[start] - 1
+    if not visited[next_node]:
+        visited[next_node] = True
+        isCycle = False
+        dfs(next_node, graph, cycle_mother)
+    else:
+        if not isCycle:
+            if next_node == start:
                 cycle_member.add(next_node)
-                
-
+                finished[next_node] = True
+            else:
+                isCycle = True
+                dfs(next_node, graph, next_node)
+        else:
+            cycle_member.add(next_node)
+            if (next_node == cycle_mother):
+                finished[next_node] = True
+                isCycle = False
+            else:
+                if not finished[next_node]:
+                    finished[next_node] = True
+                    dfs(next_node, graph, cycle_mother)
 
 for _ in range(case_cnt):
     student_cnt = int(input())
     students = list(map(int, input().split()))
     visited = [False for _ in range(student_cnt)]
-    rst = 0
+    finished = [False for _ in range(student_cnt)]
     for idx in range(student_cnt):
         if not visited[idx]:
-            dfs(idx, students)
-            print(cycle_member)
+            dfs(idx, students, -1)
+        
+    result.append(student_cnt - len(cycle_member))
     cycle_member.clear()
+
+for rst in result:
+    print(rst)
