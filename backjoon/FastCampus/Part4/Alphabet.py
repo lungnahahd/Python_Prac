071a238cb9c6ea8748726331eceda83ba50d6576
lungnahahd <jyoungjk33@gmail.com>
2024-04-12 00:14:14 +0900

    Fix : 알파벳 드디어 해결

--- backjoon/FastCampus/Part4/Alphabet.py
+++ backjoon/FastCampus/Part4/Alphabet.py
@@ -2,49 +2,36 @@
 ## 난이도 : 골드4
 
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
-now_visit = deque([])
 row, col = list(map(int, input().split()))
 
 alphabets = []
-road_cost = []
 meet_alphabet = set()
-visited = [False] * 26
 result = 0
+move_r = [-1,+1,0,0]
+move_c = [0,0,-1,+1]
 
 for _ in range(row):
     temp = list(input())
-    start_cost = [1 for _ in range(len(temp))]
     alphabets.append(temp)
-    road_cost.append(start_cost)
 
 def visiting(r,c, cost):
-    global now_visit
-    global alphabets
-    global meet_alphabet
-    global result 
+    global alphabets, meet_alphabet, result
 
-    move_r = [-1,0,+1,0]
-    move_c = [0,-1,0,+1]
-
-    #meet_alphabet.add(alphabets[r][c])
-    visited[ord(alphabets[r][c]) - 65] = True
+    result = max(result, cost)
 
     for idx in range(4):
         next_r,next_c = r + move_r[idx] , c + move_c[idx]
         if 0 <= next_r < row and 0 <= next_c < col:
-            if visited[ord(alphabets[next_r][next_c]) - 65]:
-            #if alphabets[next_r][next_c] in meet_alphabet:
-                #result = max(result, len(meet_alphabet))
-                result = max(result, cost)
-            else:
-                visited[ord(alphabets[next_r][next_c]) - 65] = True
+            if alphabets[next_r][next_c] not in meet_alphabet:
+                meet_alphabet.add(alphabets[next_r][next_c])
                 visiting(next_r, next_c, cost+1)
-                visited[ord(alphabets[next_r][next_c]) - 65] = False
-                #meet_alphabet.remove(alphabets[next_r][next_c])
+                meet_alphabet.remove(alphabets[next_r][next_c])
+
+meet_alphabet.add(alphabets[0][0])
 visiting(0,0,1)
+
 print(result)
 
