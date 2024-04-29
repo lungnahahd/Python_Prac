c296605d89e6e0365d8d9d2c3de2e7b2b060e49e
lungnahahd <jyoungjk33@gmail.com>
2024-04-30 00:36:17 +0900

    Fix : 플로이드 리스트 크기 설정 실패문제 해결 완료

--- backjoon/FastCampus/Part6/Floyd.py
+++ backjoon/FastCampus/Part6/Floyd.py
@@ -2,28 +2,32 @@
 ## 난이도 : 골드 4
 
 import sys
-INT_MAX = sys.maxsize
-input = sys.stdin.readline
+INT_MAX = int(1e9)
+#input = sys.stdin.readline
 
 cnt_city = int(input())
 cnt_bus = int(input())
 
-answer = [[INT_MAX for _ in range(cnt_bus+1)] for _ in range(cnt_bus+1)]
+answer = [[INT_MAX] * (cnt_city+1) for _ in range(cnt_city+1)]
 
 for _ in range(cnt_bus):
     start, end, cost = list(map(int, input().split()))
     answer[start][end] = min(answer[start][end], cost)
 
+for i in range(1, cnt_city+1):
+    for j in range(1, cnt_city+1):
+        if i == j:
+            answer[i][j] = 0
+
 for k in range(1, cnt_city+1):
     for i in range(1, cnt_city+1):
         for j in range(1, cnt_city+1):
-            if i == j:
-                answer[i][j] = 0
-            else:
-                temp_cost = answer[i][k] + answer[k][j]
-                answer[i][j] = min(temp_cost, answer[i][j])
+                answer[i][j] = min(answer[i][k] + answer[k][j], answer[i][j])
 
 for i in range(1, cnt_city+1):
     for j in range(1, cnt_city+1):
-        print(answer[i][j], end=' ')
+        if answer[i][j] == INT_MAX:
+            print(0, end= ' ')
+        else:
+            print(answer[i][j], end=' ')
     print()
