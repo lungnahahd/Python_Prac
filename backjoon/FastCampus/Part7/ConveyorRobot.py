e4a418482d85cb8653ce93c0add42bcba546c07e
lungnahahd <jyoungjk33@gmail.com>
2024-05-06 14:36:30 +0900

    Add : 컨베이어 벨트 위에 로봇 시간 초과 발생

--- backjoon/FastCampus/Part7/ConveyorRobot.py
+++ backjoon/FastCampus/Part7/ConveyorRobot.py
@@ -2,59 +2,48 @@
 ## 골드 5
 
 import sys
-import heapq
 from collections import deque
 input = sys.stdin.readline 
 
 size_conveyor, size_end = list(map(int, input().split()))
-conveyor = list(map(int, input().split()))
-conveyor = deque(conveyor)
+temp = list(map(int, input().split()))
+conveyor = deque([])
+for num in temp:
+    conveyor.append([num,-1])
+
 answer = 0
-robot = deque([-1 for _ in range(size_conveyor * 2)])
+#robot = deque([-1 for _ in range(size_conveyor * 2)])
 
 while True:
+    answer += 1 # 현재 단계를 카운트하는 변수
+
     #1.벨트가 각 칸 위에 로봇과 함께 한 칸 회전
     temp = conveyor.pop()
     conveyor.appendleft(temp)
-    for idx in range(size_conveyor*2):
-        if robot[idx] == -1:
+    if conveyor[size_conveyor-1][1] != -1:
+        conveyor[size_conveyor-1][1] = -1
+    
+    #2. 로봇을 벨트가 회전하는 방향으로 이동
+    for idx in range(size_conveyor-1, 0, -1):
+        if conveyor[idx][1] != -1:
             continue
-        else:
-            move = idx + 1
-            if move == size_conveyor*2:
-                move = 0
-            elif move == size_conveyor-1:
-                move = -1
-            robot[idx] = move
-
-
-    #2.로봇을 벨트가 회전하는 방향으로 이동
-    for idx in range(size_conveyor * 2):
-        if robot[idx] == -1:
-            continue
-
-        next_idx = robot[idx] + 1
-        if next_idx == size_conveyor * 2:
-            next_idx = 0
-            if conveyor[next_idx] == 0:
-                next_idx = idx
-            else:
-                conveyor[next_idx] -= 1
-                if conveyor[next_idx] == 0:
+        if conveyor[idx-1][1] != -1:
+            if conveyor[idx][0] != 0 :
+                if idx != size_conveyor - 1:
+                    conveyor[idx][1] = conveyor[idx-1][1]
+                conveyor[idx-1][1] = -1
+                conveyor[idx][0] -= 1
+                if conveyor[idx][0] == 0:
                     size_end -= 1
-        elif next_idx == size_conveyor - 1:
-            next_idx = -1
-        robot[idx] = next_idx
     
     #3.내구도가 0이 아닌 벨트에 로봇을 올림
-    if conveyor[0] != 0:
-        robot.append(0)
-        conveyor[0] -= 1
-        if conveyor[0] == 0:
+    if conveyor[0][0] != 0:
+        conveyor[0][1] = 1
+        conveyor[0][0] -= 1
+        if conveyor[0][0] == 0:
             size_end -= 1
-    
+
+    #4.내구도가 0인 칸이 조건에 맞으면 종료
     if size_end == 0:
         print(answer)
         break
-    
-    answer += 1
