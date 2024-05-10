55ccd39342dcd6274d2a1d624229de821756940c
lungnahahd <jyoungjk33@gmail.com>
2024-05-10 14:41:47 +0900

    Fix : 이차원 배열 연산 런타임 에러 발생

--- backjoon/FastCampus/Part7/TwoDimensionArrCalculate.py
+++ backjoon/FastCampus/Part7/TwoDimensionArrCalculate.py
@@ -47,10 +47,21 @@ def r_method(array):
                 row.append(0)
     return array
 
+def trasport(array):
+    origin_row, origin_col = len(array), len(array[0])
+    trasport = [[0 for _ in range(origin_row)] for _ in range(origin_col)]
+    for r in range(origin_row):
+        for c in range(origin_col):
+            trasport[c][r] = array[r][c]
+    return trasport
+
+
 def c_method(array):
     max_len = len(array) * 2
     mid_result = [[] for _ in range(max_len)]
-    
+    #mid_result = []
+
+    cut = 0
     for col_idx in range(len(array[0])):
         now_col_arr = []
         for row_idx in range(len(array)):
@@ -70,7 +81,7 @@ def c_method(array):
                 before = num
         heapq.heappush(hq, (cnt, before))
         end = 0
-        while end < max_len:
+        while hq:
             if len(hq) == 0:
                 mid_result[end].append(0)
                 mid_result[end+1].append(0)
@@ -79,6 +90,10 @@ def c_method(array):
                 mid_result[end].append(num)
                 mid_result[end+1].append(cnt)
             end += 2
+        cut = max(cut, end)
+    
+    #mid_result = mid_result[:cut]
+
     return mid_result
 
 #print(c_method(r_method(arr)))
@@ -90,15 +105,22 @@ else:
         if len(arr) >= len(arr[0]):
             arr = r_method(arr)
         else:
-            arr = c_method(arr)
+            arr = trasport(arr)
+            arr = r_method(arr)
+            arr = trasport(arr)
+            #arr = c_method(arr)
         time += 1
+        if len(arr) > 100:
+            arr = arr[:100]
+        if len(arr[0]) > 100:
+            for idx in range(len(arr)):
+                arr[idx] = arr[idx][:100]
         if want_row < len(arr) and want_col < len(arr[0]):
             if (arr[want_row][want_col] == want_value):
                 break
     
 
     if time > 100:
-        print(time)
         print(-1)
     else:
         print(time)
