6a94245f0a841260cc7c44448bfa146894adc151
lungnahahd <jyoungjk33@gmail.com>
2024-05-14 21:35:37 +0900

    Fix : 미세먼지 안녕! 시뮬레이션으로 구현 완료

--- backjoon/FastCampus/Part7/ByeDirtyDust.py
+++ backjoon/FastCampus/Part7/ByeDirtyDust.py
@@ -2,6 +2,7 @@
 ## 골드 4
 
 import sys
+import copy
 input = sys.stdin.readline
 
 row_size, col_size, time = list(map(int, input().split()))
@@ -20,23 +21,23 @@ for row in range(row_size):
     room.append(mid)
     if not find_machine:
         if temp.find('-1') != -1:
-            col = mid.index(-1)
             machine_up_row, machine_down_row = row, row + 1
-            machine_up_col, machine_down_col = col, col
+            machine_up_col, machine_down_col = 0,0
             find_machine = True
 
 
-def spin_clean_wind(row, col, room, spin_array):
-    #after_room = [[0 for _ in range(col_size)] for _ in range(row+1)]
-    after_room = [[0 for _ in range(col_size)] for _ in range(row_size)]
+def spin_clean_wind(row, col, room, spin_array, row_start, row_end):
+    after_room = copy.deepcopy(room)
     after_room[row][col] = -1
     next_r, next_c = row, col
     for move_idx in range(4):
         way = spin_array[move_idx]
         move_r, move_c = spin_dict_row[way], spin_dict_col[way]
-        while 0 <= move_r + next_r < row + 1 and 0 <= move_c + next_c < col_size:
-            if next_r != row and next_c != col :
+        while row_start <= move_r + next_r < row_end and 0 <= move_c + next_c < col_size:
+            if next_r != row or next_c != col :
                 after_room[next_r + move_r][next_c + move_c] = room[next_r][next_c]
+            else:
+                after_room[next_r + move_r][next_c + move_c] = 0
             next_r += move_r
             next_c += move_c
     after_room[row][col] = -1
@@ -53,10 +54,10 @@ def spread_dust(room, m_r, m_c):
                 move_val = room[r_idx][c_idx] // 5
                 for m_idx in range(4):
                     next_r, next_c = r_idx + move_row[m_idx], c_idx + move_col[m_idx]
-                    if 0 <= next_r < row_size and 0 <= next_c < col_size:
+                    if 0 <= next_r < row_size and 0 <= next_c < col_size and room[next_r][next_c] != -1:
                         after_room[next_r][next_c] += move_val
                         count += 1
-                after_room[r_idx][c_idx] = room[r_idx][c_idx] - move_val * count
+                after_room[r_idx][c_idx] += room[r_idx][c_idx] - move_val * count
     after_room[m_r][m_c] = -1
     after_room[m_r+1][m_c] = -1
     return after_room
@@ -64,10 +65,8 @@ def spread_dust(room, m_r, m_c):
 result = 0
 for _ in range(time):
     room = spread_dust(room, machine_up_row, machine_up_col)
-
-    room = spin_clean_wind(machine_up_row, machine_up_col, room, up_spin)
-    print(room)
-    room = spin_clean_wind(machine_down_row, machine_down_col, room, down_spin)
+    room = spin_clean_wind(machine_up_row, machine_up_col, room, up_spin, 0, machine_up_row + 1)
+    room = spin_clean_wind(machine_down_row, machine_down_col, room, down_spin, machine_down_row, row_size)
 for r in range(row_size):
     for c in range(col_size):
         if room[r][c] != -1:
