4ceb828c40b66289e78ddd55dc72ed885ff2e4d1
lungnahahd <jyoungjk33@gmail.com>
2024-05-15 19:44:20 +0900

    Fix : 경사로 시뮬레이션으로 구현 완료

--- backjoon/FastCampus/Part7/Slop.py
+++ backjoon/FastCampus/Part7/Slop.py
@@ -14,106 +14,69 @@ for _ in range(size):
 
 row_dq = deque([])
 col_dq = deque([])
+
 result = 0 
 
-for idx in range(size):
-    before = area[idx][0]
+def checkCanGo(road):
+    dq = deque([])
+    before = road[0]
     cnt = 0
-    for row_idx in range(size):
-        if before == area[idx][row_idx]:
+    for idx in range(size):
+        if before == road[idx]:
             cnt += 1
         else:
-            row_dq.append((before, cnt))
-            before = area[idx][row_idx]
+            dq.append((before, cnt))
+            before = road[idx]
             cnt = 1
-    row_dq.append((before,cnt))
+    dq.append((before, cnt))
     before_val = -1
     before_long = 0
-    no_way_go = False
-    while row_dq:
-        pass_val = False
-        now_val, now_long = row_dq.popleft()
-        if now_long == size:
-            break
-        if before_val != -1 and now_val - before_val == 1:
-            if before_long >= slop_long:
-                pass_val = True
-        before_val = now_val
-        before_long = now_long
-        if (pass_val):
-            continue
-        if len(row_dq) == 0:
-            no_way_go = True
-            break
-        else:
-            if before_val - row_dq[0][0] == 1:
-                if row_dq[0][1] >= slop_long:
-                    before_val, before_long = row_dq.popleft()
-                    continue
+    if len(dq) == 1:
+        return True
+    else:
+        before_slop_have = False
+        while dq:
+            now_val, now_long = dq.popleft()
+            if before_val != -1:
+                if now_val - before_val == 1:
+                    if before_long >= slop_long and not before_slop_have:
+                        before_val = now_val
+                        before_long = now_long
+                        before_slop_have = False
+                    else:
+                        return False
+                elif before_val - now_val == 1:
+                    if now_long >= slop_long :
+                        if now_long - slop_long >= slop_long:
+                            before_slop_have = False
+                        else:
+                            before_slop_have = True
+                        before_val = now_val
+                        before_long = now_long
+                    else:
+                        return False
                 else:
-                    no_way_go = True
-                    break
+                    return False
             else:
-                if row_dq[0][0] - before_val == 1:
-                    if before_long >= slop_long:
-                        before_val, before_long = -1, 0
-                        continue
-                no_way_go = True
-                break
-    if not no_way_go:
+                before_long = now_long
+                before_val = now_val
+        return True
+            
+for r in range(size):
+    now_road = area[r]
+    if checkCanGo(now_road):
         result += 1
+new_area = []
+for c in range(size):
+    temp = []
+    for r in range(size):
+        temp.append(area[r][c])
+    new_area.append(temp)
+  
 
-for idx in range(size):
-    before = area[0][idx]
-    cnt = 0
-    for col_idx in range(size):
-        if before == area[col_idx][idx]:
-            cnt += 1
-        else:
-            col_dq.append((before, cnt))
-            before = area[col_idx][idx]
-            cnt = 1
-    col_dq.append((before, cnt))
-    before_val = -1
-    before_long = 0
-    no_way_go = False
-    remain = 0
-    while col_dq:
-        pass_val = False
-        now_val, now_long = col_dq.popleft()
-        if now_long + remain == size:
-            break
-        if before_val != -1 and now_val - before_val == 1:
-            if before_long >= slop_long:
-                pass_val = True
-        before_val = now_val
-        before_long = now_long
-        remain += now_long
-        if (pass_val):
-            continue
-        if len(col_dq) == 0:
-            no_way_go = True
-            break
-        else:
-            if before_val - col_dq[0][0] == 1:
-                if col_dq[0][1] >= slop_long:
-                    before_val, before_long = col_dq.popleft()
-                    continue
-                else:
-                    no_way_go = True
-                    break
-            else:
-                if col_dq[0][0] - before_val == 1:
-                    if before_long >= slop_long:
-                        before_val, before_long = -1, 0
-                        continue
-                no_way_go = True
-                break
-    if not no_way_go:
-        print(col_idx, idx)
+for r in range(size):
+    now_road = new_area[r]
+    if checkCanGo(now_road):
         result += 1
-    
-
 
 print(result)
-    
