4f4c524b6cfffca656c50cc87ee9465af9f48c39
lungnahahd <jyoungjk33@gmail.com>
2024-05-29 00:46:05 +0900

    Fix : 치즈 시간 초과 발생

--- backjoon/BFS/Cheeze.py
+++ backjoon/BFS/Cheeze.py
@@ -1,62 +1,96 @@
-# 치즈
+# 치즈 (2636)
+## 난이도 : 골드 4
 
+import sys
+import copy
 from collections import deque
 
-move = [[+1,0],[0,+1],[-1,0],[0,-1]]
-row, col = map(int,input().split())
-plain = []
-cnt = 0
-for _ in range(row):
-    t_list = list(map(int,input().split()))
-    cnt += t_list.count(1)
-    plain.append(t_list)
+input = sys.stdin.readline
+move_r = [0,-1,0,+1]
+move_c = [-1,0,+1,0]
 
-def check(r,c):
-    result = False
-    for idx in range(4):
-        n_r, n_c = r + move[idx][0], c + move[idx][1]
-        if 0 <= n_r < row and 0 <= n_c < col and plain[n_r][n_c] == 0:        
-            result = True
-            break
-    return result
+row_size, col_size = list(map(int, input().split()))
 
-out_num = 0
-case_cnt = 0
-visited = [[False for _ in range(col)] for _ in range(row)]
-change = []
-#print(cnt)
-while cnt > 0:
-    out_num = 0
-    for node in change:
-        plain[node[0]][node[1]] = 0
-    change = []
-    
-    save = deque()
-    for r in range(row):
-        for c in range(col):
-            if plain[r][c] == 1 and not visited[r][c]:
-                if check(r,c):
-                    save.append((r,c))
-                    out_num += 1
-                    change.append([r,c])
-                    visited[r][c] = True
-                    break
-    while save:
-        r,c = save.popleft()
+area = []
+time = 0
+total_cheeze = 0
+out_cheeze_cnt = 0
+
+for _ in range(row_size):
+    temp = list(map(int, input().split()))
+    total_cheeze += temp.count(1)
+    area.append(temp)
+
+# 바깥 쪽에서 공기와 닿아있는 부분을 체크하는 함수
+def find_air(graph, start_r, start_c):
+    global visited
+    #out_case = copy.deepcopy(graph) # 벽과 닿아 있는 경우 : -1
+
+    can_go = deque([(start_r, start_c)])
+
+    while can_go:
+        now_r, now_c = can_go.popleft()
         for idx in range(4):
-            n_r, n_c = r + move[idx][0], c + move[idx][1]
-            if 0 <= n_r < row and 0 <= n_c < col and not visited[n_r][n_c]:
-                if plain[n_r][n_c] == 1:
-                    if check(n_r,n_c):
-                        save.append((n_r,n_c))
-                        change.append([n_r,n_c])
-                        visited[n_r][n_c] = True
-                        out_num += 1 
-    case_cnt += 1
-    cnt -= out_num
+            next_r, next_c = now_r + move_r[idx], now_c + move_c[idx]
+            if 0 <= next_r < row_size and 0 <= next_c < col_size:
+                if graph[next_r][next_c] == 0 and not visited[next_r][next_c] :
+                    graph[next_r][next_c] = -1
+                    visited[next_r][next_c] = True
+                    can_go.append((next_r, next_c))
+    
+    return graph
+
+
+def get_rid_cheeze(graph, start_r, start_c):
+    global visited, time, out_cheeze_cnt
+    
+    out_cheeze = set()
+    can_go = deque([(start_r, start_c)])
+
+
+    while can_go:
+        now_r, now_c = can_go.popleft()
+        for chk_idx in range(4):
+            chk_r, chk_c = now_r + move_r[chk_idx], now_c + move_c[chk_idx]
+            if 0 <= chk_r < row_size and 0 <= chk_c < row_size:
+                if graph[chk_r][chk_c] == -1:
+                    out_cheeze.add((now_r, now_c))
+                if graph[chk_r][chk_c] == 1 and not visited[chk_r][chk_c]:
+                    visited[chk_r][chk_c] = True
+                    can_go.append((chk_r, chk_c))
+    
+    out_cheeze_cnt += len(out_cheeze)
+    for out_r, out_c in out_cheeze:
+        graph[out_r][out_c] = -1
+    return graph
 
 
 
 
-print(case_cnt)
-print(out_num)
+for idx in range(col_size):
+    area[0][idx] = -1
+    area[row_size-1][idx] = -1
+for idx in range(row_size):
+    area[idx][0] = - 1
+    area[idx][col_size-1] = -1
+
+while total_cheeze > 0:
+    out_cheeze_cnt = 0
+    visited = [[False for _ in range(col_size)] for _ in range(row_size)]
+    for r in range(row_size):
+        for c in range(col_size):
+            if area[r][c] == -1 and not visited[r][c]:
+                visited[r][c] = True
+                area = find_air(area, r,c)
+    visited = [[False for _ in range(col_size)] for _ in range(row_size)]
+    for r in range(row_size):
+        for c in range(col_size):
+            if area[r][c] == 1 and not visited[r][c]:
+                visited[r][c] = True
+                area = get_rid_cheeze(area,r,c)
+    total_cheeze -= out_cheeze_cnt
+    time += 1
+                
+
+print(time)
+print(out_cheeze_cnt)
