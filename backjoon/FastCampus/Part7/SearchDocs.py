e4abb5c50cf6ecfd5a14fd8effbf650b912661a8
lungnahahd <jyoungjk33@gmail.com>
2024-05-30 23:11:38 +0900

    Fix : 문서 검색 문제 해결

--- backjoon/FastCampus/Part7/SearchDocs.py
+++ backjoon/FastCampus/Part7/SearchDocs.py
@@ -2,21 +2,26 @@
 ## 난이도 : 실버 5
 
 import sys
-
+INT_MAX = sys.maxsize
 docs = list(input())
 find = list(input())
+can_case = []
 
 idx = 0
 result = 0
-for word in docs:
-    if word == find[idx]:
-        idx += 1
-        if idx == len(find):
-            idx = 0
-            result += 1
-    else:
-        if word == find[0]:
-            idx = 1
-        else:
-            idx = 0
+
+start = find[0]
+find_size = len(find)
+
+for idx in range(len(docs)):
+    if docs[idx] == start and (idx + find_size -1) < len(docs):
+        can_case.append((docs[idx:idx + find_size], idx, idx+find_size-1))
+
+result = 0
+last_idx = -1
+for case in can_case:
+
+    if case[0] == find and last_idx < case[1]:
+        last_idx = case[2]
+        result += 1
 print(result)
