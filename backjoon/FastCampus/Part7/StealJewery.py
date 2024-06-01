73347469c8e9e4f8c2aef0f5a27f767fa6975565
lungnahahd <jyoungjk33@gmail.com>
2024-06-01 12:09:47 +0900

    Fix : 보석 도둑 메모리 초과 발생

--- backjoon/FastCampus/Part7/StealJewery.py
+++ backjoon/FastCampus/Part7/StealJewery.py
@@ -8,28 +8,37 @@ input = sys.stdin.readline
 cnt_jewery, cnt_bag = list(map(int, input().split()))
 
 jewery_save = []
+jewery_dict = dict()
 bag_save = []
 temp_steal = []
 
 for _ in range(cnt_jewery):
     weight, cost = list(map(int, input().split()))
     heapq.heappush(jewery_save, (-cost, -weight))
+    temp_key = str(weight) + str(cost)
+    if temp_key in jewery_dict:
+        jewery_dict[temp_key] += 1
+    else:
+        jewery_dict[temp_key] = 1
 
 for _ in range(cnt_bag):
     size = int(input())
-    heapq.heappush(bag_save, size)
+    bag_save.append(size)
+bag_save.sort()
 
-for _ in range(cnt_bag):
+can_steal = [[] for _ in range(len(bag_save))]
+while jewery_save:
     cost, weight = heapq.heappop(jewery_save)
-    heapq.heappush(temp_steal, (-weight, cost))
+    for bag_idx in range(len(bag_save)):
+        if bag_save[bag_idx] >= -weight:
+            can_steal[bag_idx].append((-cost, -weight))
 
 result = 0
-while bag_save:
-    now_weight, now_cost = heapq.heappop(temp_steal)
-    if bag_save[0] >= now_weight:
-        result += (-now_cost)
-        heapq.heappop(bag_save)
-    else:
-        append_cost, append_weight = heapq.heappop(jewery_save)
-        heapq.heappush(temp_steal, (-append_weight, append_cost))
+for now_steal in can_steal:
+    for steal in now_steal:
+        temp_key = str(steal[1]) + str(steal[0])
+        if jewery_dict[temp_key] > 0:
+            result += steal[0]
+            jewery_dict[temp_key] -= 1
+            break
 print(result)
