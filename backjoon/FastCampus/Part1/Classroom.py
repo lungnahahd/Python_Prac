ce65a1a0b0684e6a74d9e4709cbf02479b33d740
lungnahahd <jyoungjk33@gmail.com>
2024-06-06 11:04:04 +0900

    Add :  강의실 틀림

--- backjoon/FastCampus/Part1/Classroom.py
+++ backjoon/FastCampus/Part1/Classroom.py
@@ -1,34 +1,33 @@
 # 강의실 (1374)
-## 난이도 : 중
+## 난이도 : 골드 5
 
 import sys
 import heapq
-import copy
 
 input = sys.stdin.readline
-
-cnt_class = int(input())
-result = 0
+finished = set()
 remain = []
-temp = []
+studying_room = []
 
-for _ in range(cnt_class):
-    no, start, end = list(map(int, input().split()))
-    heapq.heappush(remain, (-end, -start, no))
+count_room = int(input())
+result = 0
 
-s_time = -1
-e_time = 0
-while len(remain) != 0 :
-    end_temp, start_temp, no_temp = heapq.heappop(remain)
-    if (s_time == -1 or s_time >= -end_temp):
-        s_time = -start_temp
-        e_time = -end_temp
-    else :
-        heapq.heappush(temp, (end_temp, start_temp, no_temp))
-    if len(remain) == 0:
+for _ in range(count_room):
+    num, start, end = list(map(int, input().split()))
+    heapq.heappush(remain, (-end, start, num))
+
+while remain:
+    now_end, now_start, now_num = heapq.heappop(remain)
+
+    if len(studying_room) == 0:
+        if result == 0:
+            result += 1
+        heapq.heappush(studying_room, (-start))
+        continue
+    
+    if abs(studying_room[0]) < now_start:
         result += 1
-        remain = copy.deepcopy(temp)
-        temp = []
-        s_time = -1
+        heapq.heappush(studying_room, (-start))
+    else:
+        heapq.heappop(studying_room)
 print(result)
-
