20ff9f36db81631629e56b4b1edb7350134b0d8e
lungnahahd <jyoungjk33@gmail.com>
2024-06-15 13:43:30 +0900

    Fix: 카잉 달력 로직 변경으로 시간 초과 해결 완료

--- backjoon/BruteForce/KhaingSche.py
+++ backjoon/BruteForce/KhaingSche.py
@@ -20,22 +20,12 @@ for _ in range(case_cnt):
     not_end_x, not_end_y = True, True
     idx = 0
     now_add = 0
-    while not_end_x or not_end_y:
-        if not_end_x:
-            now_add = x + max_M * idx 
-            if (now_add > max_year):
-                not_end_x = False
-            else:
-                arr_x.append(now_add)
-        if not_end_y:
-            now_add = y + max_N * idx
-            if (now_add > max_year):
-                not_end_y = False
-            else:
-                set_y.add(now_add)
-        idx += 1
     mid_result = -1
-    for num in arr_x:
-        if num in set_y:
-            mid_result = num
+    while x <= max_year:
+        if (x - y) >= 0:
+            if (x - y) % max_N == 0:
+                mid_result = x
+                break
+        x += max_M
+
     print(mid_result)
