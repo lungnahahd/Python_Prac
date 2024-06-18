53df7e35e864b73f8c44f612ce0cc6b55c7b1b19
lungnahahd <jyoungjk33@gmail.com>
2024-06-19 00:26:27 +0900

    Fix: 사이클게임 시간초과발생

--- backjoon/FastCampus/Part1/CycleGame.py
+++ backjoon/FastCampus/Part1/CycleGame.py
@@ -1,41 +1,55 @@
-# 사이클게임
-## 난이도 : 중
+# 사이클게임 (20040)
+## 난이도 : 골드 4
 
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-point_num, stage_num = list(map(int, input().split()))
+node_cnt, line_cnt = list(map(int, input().split()))
+visited = [False for _ in range(node_cnt)]
+graph = [[] for _ in range(node_cnt)]
 
-paraents = [idx for idx in range(point_num)]
 
-def find_mother(node):
-    if node == paraents[node]:
-        return paraents[node]
-    else:
-        return find_mother(paraents[node])
+def check_cicle(start, mother):
+    global visited
+    visited[start] = True
+    isCycle = False
+    result = -1
+    save = deque([start])
+    mother[start] = start
+    while save:
+        now_node = save.popleft()
+        for next_node, step in graph[now_node]:
+            if visited[next_node]:
+                if mother[now_node] != next_node:
+                    result = max(result, step)
+                    isCycle = True
+            else:
+                visited[next_node] = True
+                result = max(result, step)
+                mother[next_node] = now_node
+                save.append(next_node)
+    if not isCycle:
+        result = -1 
+    return result
 
-def union(x, y):
-    x_mother = find_mother(x)
-    y_mother = find_mother(y)
-    if(x_mother <= y_mother):
-        paraents[y] = x_mother
-    else:
-        paraents[x] = y_mother
 
-def chk_cycle(paraents, x, y):
-    if (paraents[x] == paraents[y]):
-        return True
-    else:
-        return False
+for idx in range(line_cnt):
+    a,b = list(map(int, input().split()))
+    graph[a].append((b,idx+1))
+    graph[b].append((a,idx+1))
 
-not_fin = True
-rst_stage = 0
-
-for stage in range(1,stage_num+1):
-    x, y = list(map(int, input().split()))
-    if(not_fin and chk_cycle(paraents, x, y)):
-        not_fin = False
-        rst_stage = stage
-    union(x,y)
-
-print(rst_stage)
+answer = -1
+for node in range(node_cnt):
+    if not visited[node]:
+        mother = [-1 for _ in range(node_cnt)]
+        temp_answer = check_cicle(node, mother)
+    if temp_answer != -1:
+        if answer == -1:
+            answer = temp_answer
+        else:
+            answer = min(answer, temp_answer)
+if answer == -1:
+    print(0)
+else:
+    print(answer)
