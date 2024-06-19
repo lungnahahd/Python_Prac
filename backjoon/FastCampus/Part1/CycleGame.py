7b0345270fc8f360e43e72d9b70caaa8ff88e0b0
lungnahahd <jyoungjk33@gmail.com>
2024-06-20 00:34:20 +0900

    Fix: 사이클게임 틀림처리

--- backjoon/FastCampus/Part1/CycleGame.py
+++ backjoon/FastCampus/Part1/CycleGame.py
@@ -4,52 +4,59 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 node_cnt, line_cnt = list(map(int, input().split()))
-visited = [False for _ in range(node_cnt)]
+mother = [idx for idx  in range(node_cnt)]
 graph = [[] for _ in range(node_cnt)]
+visited = [False for _ in range(node_cnt)]
 
+def change_mother(max_n, min_n):
+    global mother
+    mother[max_n] = min_n
 
-def check_cicle(start, mother):
+def find_mother(node):
+    if node == mother[node]:
+        return node
+    else:
+        return find_mother(mother[node])
+
+for step in range(line_cnt):
+    a, b = list(map(int, input().split()))
+    max_num = max(a,b)
+    min_num = min(a,b)
+    graph[min_num].append((max_num, step+1))
+    change_mother(max_num, min_num)
+
+for num in range(node_cnt):
+    mother[num] = find_mother(num)
+
+def find_cycle(start):
     global visited
     visited[start] = True
-    isCycle = False
-    result = -1
     save = deque([start])
-    mother[start] = start
+    result = -1
+    isCylce = False
     while save:
         now_node = save.popleft()
         for next_node, step in graph[now_node]:
-            if visited[next_node]:
-                if mother[now_node] != next_node:
-                    result = max(result, step)
-                    isCycle = True
-            else:
-                visited[next_node] = True
+            if not visited[next_node]:
                 result = max(result, step)
-                mother[next_node] = now_node
+                visited[next_node] = True
                 save.append(next_node)
-    if not isCycle:
-        result = -1 
+            else:
+                if mother[now_node] == mother[next_node]:
+                    result = max(result, step)
+                    isCylce = True
+    if not isCylce:
+        result = -1
     return result
 
-
-for idx in range(line_cnt):
-    a,b = list(map(int, input().split()))
-    graph[a].append((b,idx+1))
-    graph[b].append((a,idx+1))
-
-answer = -1
-for node in range(node_cnt):
-    if not visited[node]:
-        mother = [-1 for _ in range(node_cnt)]
-        temp_answer = check_cicle(node, mother)
-    if temp_answer != -1:
-        if answer == -1:
-            answer = temp_answer
-        else:
-            answer = min(answer, temp_answer)
-if answer == -1:
-    print(0)
-else:
-    print(answer)
+answer = 0
+for num in range(node_cnt):
+    if not visited[num]:
+        temp = find_cycle(num)
+        if temp != - 1 :
+            if answer == 0:
+                answer = temp
+            else:
+                answer = min(answer, temp)
+print(answer)
