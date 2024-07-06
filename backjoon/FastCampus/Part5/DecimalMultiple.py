11a8c52570f78e0bb9157c9321ed6d165899dd79
lungnahahd <jyoungjk33@gmail.com>
2024-07-07 01:41:00 +0900

    Add: 소수의곱 1차 구현 틀림

---
+++ backjoon/FastCampus/Part5/DecimalMultiple.py
@@ -0,0 +1,21 @@
+# 소수의 곱 (2014)
+## 난이도 : 골드 1
+
+import sys 
+input = sys.stdin.readline
+
+decimal_count, find_location = list(map(int, input().split()))
+decimal_nums = list(map(int, input().split()))
+check_decimal = set()
+check_decimal.add(1)
+
+for target in range(2, 28):
+    for num in decimal_nums:
+        if target % num == 0:
+            if target // num in check_decimal:
+                check_decimal.add(target)
+                find_location -= 1
+                break
+    if find_location == 0:
+        print(target)
+        break
