8251111e93ee534fa51bfce0803cf7a666ea51e8
lungnahahd <jyoungjk33@gmail.com>
2024-07-08 23:11:09 +0900

    Fix: 계단 오르기 틀림...

--- backjoon/DP/Stair_Up.py
+++ backjoon/DP/Stair_Up.py
@@ -3,37 +3,25 @@
 size = int(input())
 
 stairs = []
+
 for _ in range(size):
     s = int(input())
     stairs.append(s)
 
+mid_result = [0 for _ in range(6)]
 
-two_step = False
-if size == 1:
-    answer = stairs[0]
-else:
-    dp = [[0,0,0] for _ in range(size-1)]
-    dp[0][1] = stairs[0]
-    for step in range(1,size-1):
-        if two_step:
-            dp[step][0] = max(dp[step-1][1:])
-            two_step = False
-        else:
-            dp[step][0] = max(dp[step-1])
-            if dp[step][0] == dp[step-1][2] and dp.count(dp[step-1]) == 1:
-                two_step = True
-            else:
-                two_step = False
-        dp[step][1] = dp[step-1][0] + stairs[step]
-        dp[step][2] = dp[step-1][1] + stairs[step]
+for step_size in stairs:
+    temp_3, temp_4, temp_5 = mid_result[0], mid_result[1], mid_result[2]
+    mid_result[3], mid_result[4], mid_result[5] = temp_3, temp_4, temp_5
+    mid_result[0] = max(temp_4, temp_5)
+    mid_result[1] = step_size + temp_3
+    mid_result[2] = step_size + temp_4
 
-    # print(dp)
-    if two_step:
-        answer = stairs[-1] + dp[size-2][1]
-    else:
-        answer = stairs[-1] + max(dp[size-2][0],dp[size-2][1])
-print(answer)
-print(dp)
+print(max(mid_result[0], mid_result[1], mid_result[2]))
+
+
+
+
 ## 반례
 # 10
 # 3
