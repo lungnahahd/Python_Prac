588aac1aefa31d2125ee95cf3021245cca113ce5
lungnahahd <jyoungjk33@gmail.com>
2024-07-23 00:04:33 +0900

    Fix: 코딩은 예쁘게 반례 해결로 문제 풀이 완료

--- backjoon/FastCampus/Part8/PrettyCoding.py
+++ backjoon/FastCampus/Part8/PrettyCoding.py
@@ -8,32 +8,53 @@ count_line = int(input())
 
 origin_line = list(map(int, input().split()))
 line_format = list(map(int, input().split()))
+different_list = []
+
+for idx in range(count_line):
+    different_list.append(origin_line[idx]-line_format[idx])
 
 result = 0
 
-different_value = origin_line[0] - line_format[0]
-temp_point = abs(different_value)
-finish = False
-for idx in range(1, count_line):
-    if different_value * (origin_line[idx] - line_format[idx]) < 0:
-        result += temp_point
-        different_value = origin_line[idx] - line_format[idx]
-        temp_point = abs(different_value)
-        finish = True
-    elif different_value * (origin_line[idx] - line_format[idx]) > 0:
-        different_value = origin_line[idx] - line_format[idx]
-        temp_point = max(temp_point, abs(different_value))
-        finish = False
-    else:
-        if different_value == 0:
-            different_value = origin_line[idx] - line_format[idx]
-            temp_point = abs(different_value)
-            finish = False
+while True:
+    check = set(different_list)
+    if len(check) == 1 and different_list[0] == 0:
+        break
+
+    remove_value = abs(different_list[0])
+    save_idx = [0]
+    finished = False
+    for now in range(1, count_line):
+        if different_list[now-1] == 0:
+            remove_value = abs(different_list[now])
+            save_idx.append(now)
+            finished = False
+        elif different_list[now-1] * different_list[now] > 0:
+            save_idx.append(now)
+            remove_value = min(remove_value, abs(different_list[now]))
+            finished = False
         else:
-            result += temp_point
-            different_value = 0
-            temp_point = 0
-            finish = True
-if not finish:
-    result += temp_point
+            if different_list[now] == 0:
+                finished = True
+            else:
+                finished = False
+            while save_idx:
+                out_idx = save_idx.pop()
+                if different_list[out_idx] < 0:
+                    different_list[out_idx] += remove_value
+                elif different_list[out_idx] > 0:
+                    different_list[out_idx] -= remove_value
+            result += remove_value
+            save_idx.append(now)
+            remove_value = abs(different_list[now])
+    
+    if not finished:
+        while save_idx:
+            out_idx = save_idx.pop()
+            if different_list[out_idx] < 0:
+                different_list[out_idx] += remove_value
+            elif different_list[out_idx] > 0:
+                different_list[out_idx] -= remove_value
+        result += remove_value
+    #break
+
 print(result)
