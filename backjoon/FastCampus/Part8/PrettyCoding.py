3cf2576896aa9569101085ebcd030136af2947ee
lungnahahd <jyoungjk33@gmail.com>
2024-07-22 20:49:46 +0900

    Add: 코딩은 예쁘게 반례로 풀이 실패

---
+++ backjoon/FastCampus/Part8/PrettyCoding.py
@@ -0,0 +1,38 @@
+# 코딩은 예쁘게 (2879)
+## 난이도 : 골드 3
+
+import sys
+input = sys.stdin.readline 
+
+count_line = int(input())
+
+origin_line = list(map(int, input().split()))
+line_format = list(map(int, input().split()))
+
+result = 0
+
+different_value = origin_line[0] - line_format[0]
+temp_point = abs(different_value)
+finish = False
+for idx in range(1, count_line):
+    if different_value * (origin_line[idx] - line_format[idx]) < 0:
+        result += temp_point
+        different_value = origin_line[idx] - line_format[idx]
+        temp_point = abs(different_value)
+        finish = True
+    elif different_value * (origin_line[idx] - line_format[idx]) > 0:
+        temp_point = max(temp_point, abs(origin_line[idx] - line_format[idx]))
+        finish = False
+    else:
+        if different_value == 0:
+            different_value = origin_line[idx] - line_format[idx]
+            temp_point = different_value
+            finish = False
+        else:
+            result += temp_point
+            different_value = 0
+            temp_point = 0
+            finish = True
+if not finish:
+    result += temp_point
+print(result)
