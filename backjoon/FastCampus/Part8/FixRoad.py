ff87551663c6ceddf854ca9198780b952f06f21f
lungnahahd <jyoungjk33@gmail.com>
2024-07-26 11:39:13 +0900

    Fix: 흙길 보수하기 그리드 방식으로 구현 완료

--- backjoon/FastCampus/Part8/FixRoad.py
+++ backjoon/FastCampus/Part8/FixRoad.py
@@ -17,9 +17,14 @@ answer_count = 0
 while road:
     now_start, now_end = heapq.heappop(road)
     if now_start < before_start:
-        now_start = before_start 
-    while now_start <= now_end:
-        now_start += (bridge)
+        now_start = before_start
+    gap = now_end - now_start + 1
+    now_need = (gap//bridge)
+    answer_count += now_need
+    if (gap % bridge) != 0:
         answer_count += 1
-    before_start = now_start
+        now_need += 1
+    before_start = now_start + now_need * bridge
 print(answer_count)
+
+
