2a77529aab083a52be91c7a7ddb094b21dc9655d
lungnahahd <jyoungjk33@gmail.com>
2024-08-01 16:38:10 +0900

    Fix: 종이 접기 반례 해결로 인한 그리디 문제 구현 완료

--- backjoon/SelfPrac/PaperArt.py
+++ backjoon/SelfPrac/PaperArt.py
@@ -8,17 +8,40 @@ for _ in range(case_count):
     now = list(input())
     state_list.append(now)
 
-now_idx = 0
-while now_idx < case_count:
-    now_state = state_list[now_idx]
-    now_size = len(now_state)
+def check(now_list):
+    now_size = len(now_list)
     mid_idx = now_size // 2
+
     left_idx, right_idx = mid_idx - 1, mid_idx + 1
-    nowResult = "YES"
+    nowResult = 1
     while left_idx >= 0:
-        if (now_state[left_idx] == now_state[right_idx]):
-            nowResult = "NO"
+        if (now_list[left_idx] == now_list[right_idx]):
+            nowResult = 0
             break
         left_idx, right_idx = left_idx - 1, right_idx + 1
-    print(nowResult)
+    return nowResult
+
+answer = 1
+def main_work(now_list):
+    global answer
+    answer = min(answer, check(now_list))
+    new_left_end = len(now_list) // 2 - 1
+    if new_left_end < 0:
+        return
+    else:
+        new_rieght_start = len(now_list) // 2 + 1
+        main_work(now_list[0:new_left_end+1])
+        main_work(now_list[new_rieght_start: len(now_list)])
+
+
+
+now_idx = 0
+while now_idx < case_count:
+    answer = 1
+    main_work(state_list[now_idx])
+    if answer == 1:
+        print("YES")
+    else:
+        print("NO")
     now_idx += 1
+
