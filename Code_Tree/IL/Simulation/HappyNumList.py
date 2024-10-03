cc286013e59c3c28a11bd5bb64467d51e3ad650d
lungnahahd <jyoungjk33@gmail.com>
2024-10-03 22:31:25 +0900

    Add: 행복한 수열의 개수 반례로 해결 실패

---
+++ Code_Tree/IL/Simulation/HappyNumList.py
@@ -0,0 +1,37 @@
+# 행복한 수열의 개수
+
+import sys
+input = sys.stdin.readline
+
+size, check = list(map(int, input().split()))
+
+place = []
+for _ in range(size):
+    temp = list(map(int, input().split()))
+    place.append(temp)
+
+answer = 0
+
+# 동일 행 내부 판단
+for r in range(size):
+    cnt = 1
+    already_count = False
+    for c in range(size-1):
+        if place[r][c] == place[r][c+1]:
+            cnt += 1
+            if cnt == check:
+                answer += 1
+        else:
+            cnt = 1
+# 동일 열 내부 판단
+for c in range(size):
+    cnt = 1
+    for r in range(size-1):
+        if place[r][c] == place[r+1][c]:
+            cnt += 1
+            if cnt == check:
+                answer += 1
+        else:
+            cnt = 1
+
+print(answer)
