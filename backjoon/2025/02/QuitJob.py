941e1c0e46267c77b437efcc194e768872c819e4
lungnahahd <jyoungjk33@gmail.com>
2025-02-10 01:48:24 +0900

    Fix: 퇴사 2차_DP 활용 해결

--- backjoon/2025/02/QuitJob.py
+++ backjoon/2025/02/QuitJob.py
@@ -6,32 +6,18 @@ input = sys.stdin.readline
 
 day_cnt = int(input())
 detail = [[]]
-answer = 0
+answer = [0 for _ in range(day_cnt + 2)]
 
 for _ in range(day_cnt):
     now_long, now_val = list(map(int, input().split()))
     detail.append([now_long, now_val])
 
 
-def back_track(now_time, now_val):
-    global answer
-    print(now_time, now_val)
-    # if now_time > day_cnt:
-    #     answer = max(answer, now_val)
-    #     return
-    
-    if now_time == day_cnt:
-        if detail[now_time][0] == 1:
-            now_val += detail[now_time][1]
-        answer = max(answer, now_val)
-        return
+for day in range(1,day_cnt+1):
+    now_day = detail[day][0] + day
+    if now_day <= day_cnt + 1:
+        for next_day in range(now_day, day_cnt + 2):
+            answer[next_day] = max(answer[next_day], answer[day] + detail[day][1])
 
-    for idx in range(now_time, day_cnt+1):
-        if now_time + detail[idx][0] > day_cnt+1:
-            #answer = max(answer, now_val)
-            continue
-        else:
-            back_track(now_time  + detail[idx][0], now_val + detail[idx][1])
-
-back_track(1,0)
-print(answer)
+#print(answer)
+print(max(answer))
