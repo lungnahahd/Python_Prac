b27c95c06e57866c5d427582a6146aca70e11306
lungnahahd <jyoungjk33@gmail.com>
2025-02-23 15:43:21 +0900

    Fix : 로봇청소기 2차 구현 완료

--- backjoon/2025/02/CleanMachine.py
+++ backjoon/2025/02/CleanMachine.py
@@ -10,19 +10,22 @@ start_row, start_col, way = list(map(int, input().split()))
 visited = [[False for _ in range(col_size)] for _ in range(row_size)]
 
 move_r = [-1,0,+1,0]
-move_c = [0,-1,0,+1]
+move_c = [0,+1,0,-1]
 
 
 room = []
 for _ in range(row_size):
     room.append(list(map(int, input().split())))
 
-result = 1
-visited[start_row][start_col] = True
+result = 0
+visited[start_row][start_col] = False
 go_plan = deque([(start_row, start_col, way)])
 
 while go_plan:
     now_row, now_col, now_way = go_plan.popleft()
+    if room[now_row][now_col] == 0 and not visited[now_row][now_col]:
+        result += 1
+        visited[now_row][now_col] = True
     isCanClean = False
     for m_idx in range(4):
         next_r, next_c = now_row + move_r[m_idx], now_col + move_c[m_idx]
@@ -31,15 +34,17 @@ while go_plan:
                 isCanClean = True
                 break
     if isCanClean:
-        next_way = int((now_way + 1) % 4)
+        next_way = (now_way + 3) % 4
         next_r, next_c = now_row + move_r[next_way], now_col + move_c[next_way]
         if 0 <= next_r < row_size and 0 <= next_c < col_size:
                 if not visited[next_r][next_c] and room[next_r][next_c] == 0:
-                    result += 1
-                    visited[next_r][next_c] = True
+                    #result += 1
+                    #visited[next_r][next_c] = True
                     go_plan.append((next_r, next_c, next_way))
                 else:
                     go_plan.append((now_row, now_col, next_way))
+        else:
+            go_plan.append((now_row, now_col, next_way))
     else:
         next_r, next_c = now_row - move_r[now_way] , now_col - move_c[now_way]
         if 0 <= next_r < row_size and 0 <= next_c < col_size:
