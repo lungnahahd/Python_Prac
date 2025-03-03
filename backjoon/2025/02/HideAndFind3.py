1f90446276477f4e12744e97ef9ebf54758b09e5
lungnahahd <jyoungjk33@gmail.com>
2025-03-03 23:00:59 +0900

    Fix : 숨박꼭질 3 메모리초과 발생

--- backjoon/2025/02/HideAndFind3.py
+++ backjoon/2025/02/HideAndFind3.py
@@ -2,29 +2,23 @@
 ## 난이도 : 골드 5
 
 import sys
+from collections import deque
 input = sys.stdin.readline
 
+me, sibling = list(map(int, input().split()))
+not_visited = [True for _ in range(100001)]
 
-a, b = list(map(int, input().split()))
-start = min(a,b)
-end = max(a,b)
-result = sys.maxsize
+def backTracking(now, time):
 
+    next_go = deque([(now, time)])
 
-def back_track(start, end, cnt):
-    global result
-    
-    if start == end:
-        result = min(cnt, result)
-        return
-    if end > 100000 or end < start:
-        return
+    while next_go:
+        now_go, now_time = next_go.popleft()
+        if now_go == sibling:
+            print(now_time)
+            return
+        next_go.append((now_go * 2, now_time))
+        next_go.append((now_go + 1, now_time + 1))
+        next_go.append((now_go - 1, now_time + 1))
 
-    if end % 2 == 0:
-        back_track(start, end / 2, cnt)
-    
-    back_track(start, end+1, cnt + 1)
-    back_track(start, end-1, cnt + 1)
-
-back_track(start, end, 0)
-print(result)
+backTracking(me, 0)
